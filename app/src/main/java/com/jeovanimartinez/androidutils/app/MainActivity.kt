26ecd06860517e337dfd62aeb9d5c245b80dfa11
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-06 19:24:50 -0600

    Ajustes generales

--- app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
@@ -2,21 +2,10 @@ package com.jeovanimartinez.androidutils.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.jeovanimartinez.androidutils.Test
-import com.jeovanimartinez.androidutils.reviews.RateApp
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val a = Test.test(null)
-
-
-        RateApp.rateInGooglePlay(this@MainActivity)
-
-        Test.mensaje(this@MainActivity)
     }
 }
