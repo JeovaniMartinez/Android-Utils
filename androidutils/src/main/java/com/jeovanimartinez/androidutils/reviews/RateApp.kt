102a717d04e0a4b5f7c7a285a45083c5c65ad3f3
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-07 19:05:09 -0600

    Ajuste de documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/RateApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/RateApp.kt
@@ -25,7 +25,7 @@ object RateApp {
     /**
      * Dirige al usuario a los detalles de la aplicación en Google Play para que pueda calificar la aplicación
      * Si es posible, se abre la aplicación directamente en la app de Google Play, en caso de no ser posible, se abre
-     * en el navegador, si tampoco es posible, muestra un toast con un mensaje
+     * en el navegador, si tampoco es posible, muestra un toast con un mensaje.
      * Referencia: https://stackoverflow.com/questions/10816757/rate-this-app-link-in-google-play-store-app-on-the-phone
      *
      * @param activity actividad desde donde se llama, se usa para iniciar otras actividades
