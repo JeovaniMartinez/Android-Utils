9d583c45c3bfc57ffd878b2ea8cf5c3f98742611
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-09 23:12:02 -0600

    Ajustes a RateInApp

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
@@ -1,51 +1,59 @@
 package com.jeovanimartinez.androidutils.reviews.rateinapp
 
-import com.jeovanimartinez.androidutils.BuildConfig
+import com.jeovanimartinez.androidutils.Base
 
 /**
- * Utilidad para iniciar el flujo que invita al usuario a calificar la aplicación.
+ * Utilidad para iniciar el flujo que invita al usuario a calificar la aplicación, en base a ciertas condiciones sobre el uso de la aplicación.
  * Para Android 5.0 (API 21) y posteriores, se utiliza Google Play In-App Review API, por lo que se puede calificar dentro de la aplicación.
  * Para versiones anteriores a Android 5.0, se muestra un diálogo para invitar al usuario a calificar la aplicación, si el usuario acepta, es
  * dirigido a los detalles de la aplicación en Google Play.
  * */
-class RateInApp {
+object RateInApp : Base<RateInApp>() {
 
-    companion object {
-
-        /**
-         * Número mínimo de días requeridos desde que se instalo la app para poder mostrar el flujo, se usa en combinación con minLaunchTimes,
-         * y se deben cumplir ambas condiciones para mostrar el flujo
-         * */
-        var minInstallElapsedDays = 0
-
-        /**
-         * Número mínimo de veces que se ha iniciado la app desde que se instalo para poder mostrar el flujo, se usa en combinación con
-         * minInstallElapsedDays, y se deben cumplir ambas condiciones para mostrar el flujo
-         * */
-        var minLaunchTimes = 0
-
-        /** Para habilitar o deshabilitar los mensajes del log */
-        var logEnable = BuildConfig.DEBUG
-
-        /** Determina si ya se llamo al método build(), ya que es necesario antes de llamar cualquier otro método */
-        private var builded = false
-
-        /**
-         * Método para configurar la utilidad, debe llamarse solo una vez al iniciarse la aplicación
-         * */
-        fun build() {
-
-            builded = true
-        }
-
-    }
+    override val LOG_TAG = "RateInApp"
 
     /** Objeto con las constantes para las preferencias */
-    private object Preferences {
+    object Preferences {
         const val KEY = "rate_in_app_preferences"
         const val LAUNCH_COUNTER = "rate_in_app_launch_counter"
         const val LAST_SHOW_DATE = "rate_in_app_last_show_date"
-        const val NEVER_SHOW_AGAIN = "rate_in_app_never_show_again"
+        const val NEVER_SHOW_AGAIN = "rate_in_app_never_show_again" // Aplica solo para el mensaje de versiones anteriores a Android 5
     }
+
+    private var minInstallElapsedDays = 4
+
+    /**
+     * Establece el número mínimo de días requeridos desde que se instalo la app para poder mostrar el flujo, se usa en combinación con minInstallLaunchTimes,
+     * y se deben cumplir ambas condiciones para mostrar el flujo
+     * */
+    fun setMinInstallElapsedDays(minInstallElapsedDays: Int): RateInApp {
+        this.minInstallElapsedDays = minInstallElapsedDays
+        return this
+    }
+
+
+    private var minInstallLaunchTimes = 5
+
+    /**
+     * Establece el número mínimo de veces que se debe haber iniciado la app desde que se instalo para poder mostrar el flujo, se usa en combinación con
+     * minInstallElapsedDays, y se deben cumplir ambas condiciones para mostrar el flujo
+     * */
+    fun setMinInstallLaunchTimes(minInstallLaunchTimes: Int): RateInApp {
+        this.minInstallLaunchTimes = minInstallLaunchTimes
+        return this
+    }
+
+
+    /**
+     * Número mínimo de días requeridos desde que se mostró el flujo para mostrarlo nuevamente, se usa en combinación con minRemindLaunchTimes,
+     * y se deben cumplir ambas condiciones para mostrar el flujo
+     * */
+    private var minRemindElapsedDays = 2
+
+    /**
+     * Número mínimo de veces que se debe haber iniciado la app desde que mostró el flujo para mostrarlo nuevamente, se usa en combinación con
+     * minRemindElapsedDays, y se deben cumplir ambas condiciones para mostrar el flujo
+     * */
+    private var minRemindLaunchTimes = 3
 
 }
--- app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
@@ -3,8 +3,10 @@ package com.jeovanimartinez.androidutils.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.jeovanimartinez.androidutils.moreapps.MoreAppsGPlay
 import com.jeovanimartinez.androidutils.reviews.RateApp
+import com.jeovanimartinez.androidutils.reviews.rateinapp.RateInApp
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,12 +31,25 @@ class MainActivity : AppCompatActivity() {
 
         // Calificar en google play
         rateInGooglePlayBtn.setOnClickListener{
+
+            /*
+            * Para asignar la instancia de FirebaseAnalytics, que permite que algunas clases registren eventos
+            * Es necesario que la aplicación tenga configurado FirebaseAnalytics
+            * */
+            RateApp
+                .setLogEnable(true)
+                .setFirebaseAnalyticsInstance(FirebaseAnalytics.getInstance(this@MainActivity))
+
             RateApp.goToRateInGooglePlay(this@MainActivity)
+
         }
 
         // Más aplicaciones
         moreAppsBtn.setOnClickListener {
-            MoreAppsGPlay.setLogEnable(true).setDeveloperId("INTSIG+Information+Co.,Ltd").showAppList(this@MainActivity)
+            MoreAppsGPlay.setDeveloperId("Jedemm+Technologies").showAppList(this@MainActivity)
+
+            RateInApp
+                .setMinInstallElapsedDays(5)
         }
 
 
