7e3200278f92704126a22e62e77ff0bc15170a86
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-10 14:05:30 -0600

    Se agregan validaciones

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
@@ -1,5 +1,6 @@
 package com.jeovanimartinez.androidutils.reviews.rateinapp
 
+import android.content.Context
 import com.jeovanimartinez.androidutils.Base
 
 /**
@@ -28,10 +29,10 @@ object RateInApp : Base<RateInApp>() {
      * y se deben cumplir ambas condiciones para mostrar el flujo
      * */
     fun setMinInstallElapsedDays(minInstallElapsedDays: Int): RateInApp {
+        validateConfigArgument(minInstallElapsedDays)
         this.minInstallElapsedDays = minInstallElapsedDays
         return this
     }
-
 
     private var minInstallLaunchTimes = 5
 
@@ -40,29 +41,46 @@ object RateInApp : Base<RateInApp>() {
      * minInstallElapsedDays, y se deben cumplir ambas condiciones para mostrar el flujo
      * */
     fun setMinInstallLaunchTimes(minInstallLaunchTimes: Int): RateInApp {
+        validateConfigArgument(minInstallLaunchTimes)
         this.minInstallLaunchTimes = minInstallLaunchTimes
         return this
     }
 
+    private var minRemindElapsedDays = 2
 
     /**
      * Número mínimo de días requeridos desde que se mostró el flujo para mostrarlo nuevamente, se usa en combinación con minRemindLaunchTimes,
      * y se deben cumplir ambas condiciones para mostrar el flujo
      * */
-    private var minRemindElapsedDays = 2
+    fun setMinRemindElapsedDays(minRemindElapsedDays: Int): RateInApp {
+        validateConfigArgument(minRemindElapsedDays)
+        this.minRemindElapsedDays = minRemindElapsedDays
+        return this
+    }
+
+    private var minRemindLaunchTimes = 3
 
     /**
      * Número mínimo de veces que se debe haber iniciado la app desde que mostró el flujo para mostrarlo nuevamente, se usa en combinación con
      * minRemindElapsedDays, y se deben cumplir ambas condiciones para mostrar el flujo
      * */
-    private var minRemindLaunchTimes = 3
+    fun setMinRemindLaunchTimes(minRemindLaunchTimes: Int): RateInApp {
+        validateConfigArgument(minRemindLaunchTimes)
+        this.minRemindLaunchTimes = minRemindLaunchTimes
+        return this
+    }
 
     /**
-    * Inicializa y configura la utilidad, debe llamarse siempre solo una vez en la aplicación
-    * ya sea en el onCreate() de la app o de la actividad principal
-    * */
+     * Inicializa y configura la utilidad, debe llamarse siempre solo una vez en la aplicación
+     * ya sea en el onCreate() de la app o de la actividad principal
+     * */
     fun init(context: Context) {
 
+    }
+
+    /** Se asegura que el argumento de configuración [value] sea válido */
+    private fun validateConfigArgument(value: Int) {
+        if (value < 0) throw IllegalArgumentException("Value must be equal to or greater than zero")
     }
 
 }
