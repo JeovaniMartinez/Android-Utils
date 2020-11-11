41ebe71427c3d631b9b93c6be61cf76d463bea1b
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-10 18:49:44 -0600

    Ajustes de diseño y funcionamiento

--- app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jeovanimartinez.androidutils.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.jeovanimartinez.androidutils.moreapps.MoreAppsGPlay
 import com.jeovanimartinez.androidutils.reviews.RateApp
 import com.jeovanimartinez.androidutils.reviews.rateinapp.RateInApp
@@ -31,25 +30,12 @@ class MainActivity : AppCompatActivity() {
 
         // Calificar en google play
         rateInGooglePlayBtn.setOnClickListener{
-
-            /*
-            * Para asignar la instancia de FirebaseAnalytics, que permite que algunas clases registren eventos
-            * Es necesario que la aplicación tenga configurado FirebaseAnalytics
-            * */
-            RateApp
-                .setLogEnable(true)
-                .setFirebaseAnalyticsInstance(FirebaseAnalytics.getInstance(this@MainActivity))
-
             RateApp.goToRateInGooglePlay(this@MainActivity)
-
         }
 
         // Más aplicaciones
         moreAppsBtn.setOnClickListener {
-            MoreAppsGPlay.setDeveloperId("Jedemm+Technologies").showAppList(this@MainActivity)
-
-            RateInApp
-                .setMinInstallElapsedDays(5)
+            MoreAppsGPlay.showAppList(this@MainActivity)
         }
 
 
