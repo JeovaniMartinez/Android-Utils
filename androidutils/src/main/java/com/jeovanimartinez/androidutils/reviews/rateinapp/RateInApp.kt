a97f46d9481b8ef8343f3535911ab44c9e5fccae
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-10 19:25:35 -0600

    Se agrega showAtEvent

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateInApp.kt
@@ -75,6 +75,20 @@ object RateInApp : Base<RateInApp>() {
         return this
     }
 
+    private var showAtEvent = 2
+
+    /**
+     * Cuando se llama a checkAndShow() se va a mostrar el flujo si se cumplen las condiciones, setShowAtEvent permite modificar a las cuantas veces
+     * que se llama a checkAndShow() se muestre el flujo. Por ejemplo, se desea mostrar el flujo para calificar en el onResume() de la actividad principal,
+     * pero la tercer vez que se llame a ese método, en ese caso, establecer showAtEvent = 3, que hará que se muestre el flujo hasta la tercera vez que se llame
+     * a onResume(). Si de otro modo, showAtEvent fuera 1, el flujo se mostraría en la primera llamada a onResume(). El valor mínimo de showAtEvent es 1
+     * */
+    fun setShowAtEvent(showAtEvent: Int): RateInApp {
+        validateConfigArgument(showAtEvent, 1)
+        this.showAtEvent = showAtEvent
+        return this
+    }
+
     /** Se asegura que el argumento de configuración [value] sea válido */
     private fun validateConfigArgument(value: Int, minValue: Int) {
         if (value < minValue) throw IllegalArgumentException("Invalid config value, value ($value) must be equal to or greater than $minValue")
@@ -103,6 +117,7 @@ object RateInApp : Base<RateInApp>() {
             minInstallLaunchTimes: $minInstallLaunchTimes
             minRemindElapsedDays: $minRemindElapsedDays
             minRemindLaunchTimes: $minRemindLaunchTimes
+            showAtEvent: $showAtEvent
         """.trimIndent()
         )
     }
