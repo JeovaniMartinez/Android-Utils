51e532b341f28acf34cb2a120d2678ac79fc4119
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-21 20:40:47 -0600

    Se finaliza el diseño de la actividad para invitar al usuario a calificar la app

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateAppActivity.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/reviews/rateinapp/RateAppActivity.kt
@@ -1,11 +1,14 @@
 package com.jeovanimartinez.androidutils.reviews.rateinapp
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Fade
 import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.DrawableCompat
 import com.jeovanimartinez.androidutils.R
+import kotlinx.android.synthetic.main.activity_rate_app.*
 
 /** Actividad que se muestra en forma de diálogo para invitar al usuario a calificar la aplicación */
 class RateAppActivity : AppCompatActivity() {
@@ -14,6 +17,25 @@ class RateAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         configureTransitions()
         setContentView(R.layout.activity_rate_app)
+
+        initSetup()
+    }
+
+    /** Configuración inicial */
+    private fun initSetup() {
+        configureTopShapeBackground()
+    }
+
+    /**
+     * Asigna el color de fondo de la tarjeta al fondo de la forma superior, para mantener soporte para tema claro y oscuro
+     * Referencia: https://stackoverflow.com/questions/11376516/change-drawable-color-programmatically/11376812
+     * */
+    private fun configureTopShapeBackground() {
+        val topShapeBackground = AppCompatResources.getDrawable(this@RateAppActivity, R.drawable.rate_app_top_shape)
+        val finalTopShapeBackground = DrawableCompat.wrap(topShapeBackground!!)
+        DrawableCompat.setTint(finalTopShapeBackground, rateApp_card.cardBackgroundColor.defaultColor)
+
+        rateApp_topShape.background = finalTopShapeBackground
     }
 
     /** Configura la transición de entrada y salida */
