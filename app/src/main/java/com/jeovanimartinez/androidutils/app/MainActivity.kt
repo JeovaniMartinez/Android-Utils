e716b66805972815b0c94b508654efa13a35e0f0
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-11-22 18:08:15 -0600

    Ajustes para probar la biblioteca

--- app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.jeovanimartinez.androidutils.app
 
-import android.app.ActivityOptions
-import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.jeovanimartinez.androidutils.moreapps.MoreAppsGPlay
 import com.jeovanimartinez.androidutils.reviews.RateApp
-import com.jeovanimartinez.androidutils.reviews.rateinapp.RateAppActivity
 import com.jeovanimartinez.androidutils.reviews.rateinapp.RateInApp
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -29,27 +23,34 @@ class MainActivity : AppCompatActivity() {
     private fun initSetup() {
 
         // Alternar los temas
-        toggleThemeBtn.setOnClickListener {
+        toggleTheme.setOnClickListener {
+
             if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             }
+
         }
 
         // Calificar en google play
-        rateInGooglePlayBtn.setOnClickListener {
+        rateInGooglePlay.setOnClickListener {
+
             RateApp.goToRateInGooglePlay(this@MainActivity)
+
         }
 
         // Más aplicaciones
-        moreAppsBtn.setOnClickListener {
+        moreApps.setOnClickListener {
+
             MoreAppsGPlay.showAppList(this@MainActivity)
+
         }
 
-        initRateInAppBtn.setOnClickListener {
+        // Iniciar y configurar la utilidad para mostrar el flujo para calificar la app
+        initRateInApp.setOnClickListener {
+
             RateInApp
-                .setLogEnable(true)
                 .setMinInstallElapsedDays(0)
                 .setMinInstallLaunchTimes(1)
                 .setMinRemindElapsedDays(0)
@@ -62,7 +63,8 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        checkAndShowRateInAppBtn.setOnClickListener {
+        // Muestra el flujo para calificar la app (si se cumplen las condiciones de la configuración)
+        checkAndShowRateInApp.setOnClickListener {
 
             if (!rateInAppConfigured) {
                 Toast.makeText(this@MainActivity, "Please initialize RateInApp before to click this button", Toast.LENGTH_SHORT).show()
@@ -70,6 +72,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             RateInApp.checkAndShow(this@MainActivity)
+
         }
 
     }
