9c1060175a78084023eb8594e1c542dcbd672c7e
Jeovani Martínez <jeovani_martinez@hotmail.com>
2020-12-09 18:19:39 -0600

    Ajuste de propiedades

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutApp.kt
@@ -7,36 +7,42 @@ import android.os.Build
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.jeovanimartinez.androidutils.R
 
 /** Utilidad para mostrar una pantalla de acerca de la aplicación */
 object AboutApp {
 
-    /** Color de fondo de la pantalla (id del color) */
-    @ColorRes
-    var backgroundColor: Int? = null
+    /** Ícono de la aplicación */
+    @DrawableRes
+    var appIcon = R.drawable.library_logo
 
     /** Nombre de la aplicación */
-    var appName: String? = null
-
-    /** Ícono de la aplicación (id del recurso) */
-    @DrawableRes
-    var appIcon: Int? = null
+    @StringRes
+    var appName = R.string.about_app_app_name
 
     /** Nombre del autor */
-    var authorName: String? = null
+    @StringRes
+    var authorName = R.string.about_app_author_name
 
-    /** URL que se abre al dar clic al nombre del autor  */
-    var authorLink: String? = null
+    /** URL que se abre al dar clic al nombre del autor */
+    @StringRes
+    var authorLink = R.string.about_app_author_link
 
-    /** Logo de la empresa (id del recurso) */
+    /** Logo de la empresa */
     @DrawableRes
-    var companyLogo: Int? = null
+    var companyLogo = R.drawable.logo_jedem
 
-    /** URL que se abre al dar clic al logo de la empresa  */
-    var companyLink: String? = null
+    /** Nombre de la empresa */
+    @StringRes
+    var companyName = R.string.about_app_company_name
+
+    /** URL que se abre al dar clic al logo de la empresa */
+    @StringRes
+    var companyLink = R.string.about_app_company_link
 
     /** URL de los términos de uso y política de privacidad */
-    var termsAndPrivacyPolicyLink: String? = null
+    @StringRes
+    var termsAndPrivacyPolicyLink = R.string.about_app_terms_and_policy_link
 
     /**
      * Determina si se muestra un botón para ver las licencias de código abierto de la app.
@@ -44,7 +50,7 @@ object AboutApp {
      * y el build.gradle del proyecto debe implementar el plugin 'com.google.android.gms.oss-licenses-plugin', tal como se muestra en la documentación
      * oficial: https://developers.google.com/android/guides/opensource
      * */
-    var showOpenSourceLicenses = false
+    var showOpenSourceLicenses = true
 
     /**
      * Si se desea configurar la TaskDescription para la actividad de acerca de, asignar las propiedades del objeto,
