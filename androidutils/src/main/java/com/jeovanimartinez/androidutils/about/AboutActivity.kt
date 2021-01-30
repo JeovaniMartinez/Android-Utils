3a35347c7092c90534ef6c8111c3fc6a0582e5c8
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-01-29 21:24:47 -0600

    Ajustes generales y para mantener el estado de la vista

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutActivity.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutActivity.kt
@@ -25,6 +25,10 @@ import java.util.*
 /** Actividad de acerca de */
 class AboutActivity : AppCompatActivity() {
 
+    companion object {
+        const val STATE_TERMS_AND_POLICY_VISIBLE = "state_terms_and_policy_visible"
+    }
+
     private var activityIsRunning = true // Para mostrar toast solo si la actividad esta en ejecución
     private var loadingTermsAndPolicyInProgress = false
     private var termsAndPolicyVisible = false
@@ -34,7 +38,7 @@ class AboutActivity : AppCompatActivity() {
         configureTransitions()
         setContentView(R.layout.activity_about)
 
-        AboutApp.log("Started About Activity")
+        AboutApp.log("Started AboutActivity")
 
         initSetup()
         configureByAboutApp()
@@ -57,6 +61,19 @@ class AboutActivity : AppCompatActivity() {
         super.onStop()
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(STATE_TERMS_AND_POLICY_VISIBLE, termsAndPolicyVisible) // Se guarda si están visibles, para el onRestoreInstanceState
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Se muestran los términos y política si estaban visibles
+        if (savedInstanceState.getBoolean(STATE_TERMS_AND_POLICY_VISIBLE)) {
+            loadTermsAndPolicy(false)
+        }
+    }
+
     /** Configuración inicial */
     private fun initSetup() {
 
@@ -71,12 +88,21 @@ class AboutActivity : AppCompatActivity() {
             about_topActionCard.translationY = dip(48).toFloat()
         }
 
-        about_closeBtn.setOnClickListener {
-            supportFinishAfterTransition()
+        about_termsAndPolicy.setOnClickListener {
+            loadTermsAndPolicy()
         }
+
 
         about_closeTermsBtn.setOnClickListener {
             hideTermsAndPolicy()
+        }
+
+        about_openSourceLicenses.setOnClickListener {
+            startActivity(Intent(this@AboutActivity, OssLicensesMenuActivity::class.java))
+        }
+
+        about_closeBtn.setOnClickListener {
+            supportFinishAfterTransition()
         }
 
     }
@@ -106,14 +132,6 @@ class AboutActivity : AppCompatActivity() {
             about_closeTermsBtn.setImageDrawable(closeTermsDrawable)
         }
 
-        about_openSourceLicenses.setOnClickListener {
-            startActivity(Intent(this@AboutActivity, OssLicensesMenuActivity::class.java))
-        }
-
-        about_termsAndPolicy.setOnClickListener {
-            loadTermsAndPolicy()
-        }
-
     }
 
     /** Configura la versión de la app y el copyright */
@@ -141,8 +159,10 @@ class AboutActivity : AppCompatActivity() {
     /**
      * Inicia el proceso para obtener los términos y la política de privacidad en un el web view, ya que se obtienen desde el servidor para mostrar la
      * versión más reciente
+     * @param animateShowTermsView determina si cuando se muestre la vista de los términos se va a hacer con una animación, esta vista solo se muestra
+     *        si se obtuvieron los datos correctamente del servidor
      * */
-    private fun loadTermsAndPolicy() {
+    private fun loadTermsAndPolicy(animateShowTermsView: Boolean = true) {
 
         // Si ya están en proceso de carga, no es necesario hacerlo nuevamente
         if (loadingTermsAndPolicyInProgress) {
@@ -178,7 +198,7 @@ class AboutActivity : AppCompatActivity() {
                 }, 500)
 
                 about_progressBar.visibility = View.GONE // Se oculta al terminar la petición
-                if (pageLoadSuccessful) showTermsAndPolicy() // Solo se muestran si la página se cargo correctamente
+                if (pageLoadSuccessful) showTermsAndPolicy(animateShowTermsView) // Solo se muestran si la página se cargo correctamente
             }
 
             override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
@@ -203,33 +223,57 @@ class AboutActivity : AppCompatActivity() {
         about_termsAndPolicyWebView.loadUrl("${getString(AboutApp.termsAndPrivacyPolicyLink)}?background-color=$backgroundColor&text-color=$textColor")
     }
 
-    /** Muestra los términos y la política de privacidad, llamar solo si se cargaron correctamente */
-    private fun showTermsAndPolicy() {
+    /** Muestra los términos y la política de privacidad, llamar solo si se cargaron correctamente, [animate] determina si se muestran con una animación o no */
+    private fun showTermsAndPolicy(animate: Boolean = true) {
 
         AboutApp.log("showTermsAndPolicy() Invoked")
         termsAndPolicyVisible = true // Indica que están visibles
 
-        // Visibilidad y animación del botón de atrás en los términos
-        about_topActionCard.visibility = View.VISIBLE
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            about_topActionCard.animate().translationX(0f).onAnimationEnd { }
+        if (animate) {
+
+            // Visibilidad y animación del botón de atrás en los términos
+            about_topActionCard.visibility = View.VISIBLE
+            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                about_topActionCard.animate().translationX(0f).onAnimationEnd { }
+            } else {
+                about_topActionCard.animate().translationY(0f).onAnimationEnd { }
+            }
+
+            about_termsAndPolicyWebView.visibility = View.VISIBLE
+
+            // Se manda al final de la vista para mostrar una animación
+            about_termsAndPolicyWebView.translationX = about_contentCard.width.toFloat()
+            about_termsAndPolicyWebView.visibility = View.VISIBLE
+
+            // Se muestra el web view y se oculta el contenedor de la tarjeta mediante una animación
+            about_contentCardLayout.animate().translationX(-about_contentCard.width.toFloat()).start()
+            about_termsAndPolicyWebView.animate().translationX(0f).start()
+
         } else {
-            about_topActionCard.animate().translationY(0f).onAnimationEnd { }
+
+            // Visibilidad y animación del botón de atrás en los términos
+            about_topActionCard.visibility = View.VISIBLE
+            if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                about_topActionCard.translationX = 0f
+            } else {
+                about_topActionCard.translationY = 0f
+            }
+
+            about_termsAndPolicyWebView.visibility = View.VISIBLE
+
+            // Se manda al final de la vista para la animación al salir de la vista
+            about_termsAndPolicyWebView.translationX = about_contentCard.width.toFloat()
+            about_termsAndPolicyWebView.visibility = View.VISIBLE
+
+            // Se muestra el web view y se oculta el contenedor de la tarjeta
+            about_contentCardLayout.translationX = -about_contentCard.width.toFloat()
+            about_termsAndPolicyWebView.translationX = 0f
+
         }
-
-        about_termsAndPolicyWebView.visibility = View.VISIBLE
-
-        // Se manda al final de la vista para mostrar una animación
-        about_termsAndPolicyWebView.translationX = about_contentCard.width.toFloat()
-        about_termsAndPolicyWebView.visibility = View.VISIBLE
-
-        // Se muestra el web view y se oculta el contenedor de la tarjeta mediante una animación
-        about_contentCardLayout.animate().translationX(-about_contentCard.width.toFloat()).start()
-        about_termsAndPolicyWebView.animate().translationX(0f).start()
 
     }
 
-    /** Oculta la vista de los términos y la política de privacidad solo */
+    /** Oculta la vista de los términos y la política de privacidad */
     private fun hideTermsAndPolicy() {
         AboutApp.log("hideTermsAndPolicy() Invoked")
         termsAndPolicyVisible = false // Se indica que ya no están visibles
