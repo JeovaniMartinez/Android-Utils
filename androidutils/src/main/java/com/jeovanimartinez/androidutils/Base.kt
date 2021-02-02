16f89641e9dda675ff2685feb8f3b95100c30b76
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-01 19:14:20 -0600

    Se optimiza la manera de manejar Firebase Analytics

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/Base.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/Base.kt
@@ -2,7 +2,9 @@
 
 package com.jeovanimartinez.androidutils
 
+import android.os.Bundle
 import android.util.Log
+import androidx.annotation.Size
 import com.google.firebase.analytics.FirebaseAnalytics
 
 /**
@@ -16,19 +18,52 @@ abstract class Base<T : Base<T>> {
          * esta configuración aplica para todas las clases que hereden de Base, es decir se activa o desactiva de manera global
          **/
         var logEnable = BuildConfig.DEBUG
+
+        /**
+         * Instancia de Firebase Analytics, asignar solo si se requiere que la app que implementa la biblioteca registre
+         * los eventos de análisis, esta propiedad es global y se usa en todas las subclases de esta clase. Si se desea
+         * desactivar el registro de eventos para una clase en específico, hacerlo mediante la propiedad firebaseAnalyticsEnabled
+         * de la instancia de la clase
+         * */
+        var firebaseAnalyticsInstance: FirebaseAnalytics? = null
     }
+
+    /**
+     * Determina si el registro de eventos en Firebase Analytics esta habilitado para la instancia de la clase, para el registro de eventos,
+     * la propiedad estática del companion object firebaseAnalyticsInstance también debe estar asignada, si esa propiedad es null, no se van
+     * a registrar eventos, ya que es requerida
+     * */
+    var firebaseAnalyticsEnabled = true
 
     /** Etiqueta par el log */
     @Suppress("PropertyName")
     protected abstract val LOG_TAG: String
 
-    /** Instancia de Firebase Analytics para registro de eventos, asignar solo si se requiere */
-    protected var firebaseAnalytics: FirebaseAnalytics? = null
+    /**
+     * Registra un evento en Firebase Analytics, siempre y cuando la clase base tenga una instancia de Firebase Analytics,
+     * y el registro de eventos este habilitado para la instancia de la clase
+     *
+     * @param eventName Nombre del evento, debe estar entre 1 y 40 caracteres
+     * @param eventParams Parámetros para el evento (opcional)
+     * */
+    internal fun firebaseAnalytics(@Size(min = 1L, max = 40L) eventName: String, eventParams: Bundle? = null) {
+        log("Event emitted: [ $eventName ]")
 
-    /** Asigna la instancia de [firebaseAnalytics] para registro de eventos */
-    fun setFirebaseAnalyticsInstance(firebaseAnalytics: FirebaseAnalytics): T {
-        this.firebaseAnalytics = firebaseAnalytics
-        return this as T
+        // Si no hay instancia de Firebase Analytics,
+        if (firebaseAnalyticsInstance == null) {
+            log("No need to log event into Firebase Analytics, firebaseAnalyticsInstance is null")
+            return
+        }
+
+        // Si el log esta deshabilitado para esta clase en específico
+        if (!firebaseAnalyticsEnabled) {
+            log("No need to log event into Firebase Analytics, this is disabled for this class instance")
+            return
+        }
+
+        // De otro modo, se procede a registrar el evento en Firebase Analytics
+        firebaseAnalyticsInstance?.logEvent(eventName, eventParams)
+        log("Event logged into Firebase Analytics")
     }
 
     /**
