be3f4a6779984d53e4b7676488d96d22cd11f48f
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-02 18:17:14 -0600

    Ajuste de documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/nullability/NullSafety.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/nullability/NullSafety.kt
@@ -10,7 +10,8 @@ package com.jeovanimartinez.androidutils.extensions.nullability
 /**
  * Ejecuta el código del bloque solo si la variable no es null. Dentro de la función, siempre usar it para hacer referencia al valor de la variable. Por ejemplo:
  *  demo.whenNotNull {
- *      Log.d("Check Nullability", it) // Usar siempre it en lugar del nombre de la variable, ya que la variable puede ser null, it nunca lo va a ser si entra al bloque
+ *      // Usar siempre it en lugar del nombre de la variable, ya que la variable puede ser null, it nunca lo va a ser si entra al bloque
+ *      Log.d("Check Nullability", it)
  *  }
  * Funciona de igual manera si se llama con el operador de llamada segura (?) o sin el (demo.whenNotNull es equivalente a demo?.whenNotNull).
  * Para efectos prácticos, se recomienda llamar sin el aperador de llamada segura.
