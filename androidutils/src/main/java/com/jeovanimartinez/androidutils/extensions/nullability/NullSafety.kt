7610f001e59dce2819704ba8da0b34d27c9974e5
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-03 18:36:01 -0600

    Se agregan extensiones para verificar si una variable es null o no

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/nullability/NullSafety.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/nullability/NullSafety.kt
@@ -19,3 +19,25 @@ package com.jeovanimartinez.androidutils.extensions.nullability
 fun <T : Any> T?.whenNotNull(f: (it: T) -> Unit) {
     if (this != null) f(this)
 }
+
+/**
+ * Verifica si la variable es null. Devuelve true si la variable es null, y false si la variable no es null.
+ * Invocar siempre sin el operador de llamada segura (?).
+ * Ejemplos:
+ *          val a: String? = null // a.isNull() = true;
+ *          val b: String? = "demo" // b.isNull() = false;
+ * */
+fun Any?.isNull(): Boolean {
+    return this == null
+}
+
+/**
+ * Verifica si la variable es null. Si la variable es null devuelve false, si no es null devuelve true.
+ * Invocar siempre sin el operador de llamada segura (?).
+ * Ejemplos:
+ *          val a: String? = null // a.isNotNull() = false;
+ *          val b: String? = "demo" // b.isNotNull() = true;
+ * */
+fun Any?.isNotNull(): Boolean {
+    return this != null
+}
