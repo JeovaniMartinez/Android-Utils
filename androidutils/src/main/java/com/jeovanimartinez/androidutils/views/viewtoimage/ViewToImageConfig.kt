4d2a5067357a96d7f5beb37fe5f92d690d3fbb0c
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-07 20:31:42 -0600

    Se agregan clases de prueba

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImageConfig.kt
@@ -0,0 +1,13 @@
+package com.jeovanimartinez.androidutils.views.viewtoimage
+
+
+
+data class ViewToImageConfig(
+
+    /** Color de fondo, debe ser el color en hexadecimal (string) o el id del recurso de colors (int) */
+    var backgroundColor: Any,
+
+
+)
+
+
