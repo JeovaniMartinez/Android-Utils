10b416f929680b16cd54449f67a6e0bcb9e365bd
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-07 18:40:27 -0600

    Se agrega la propiedad estática firebaseCrashlyticsInstance

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/Base.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/Base.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.annotation.Size
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.jeovanimartinez.androidutils.extensions.nullability.whenNotNull
 
 /**
@@ -25,6 +26,17 @@ abstract class Base<T : Base<T>> {
          * de la instancia de la clase
          * */
         var firebaseAnalyticsInstance: FirebaseAnalytics? = null
+
+        /**
+         * Instancia de Firebase Crashlytics, asignar solo si se requiere que la app que implementa la biblioteca registre
+         * errores en Firebase Crashlytics, esta propiedad es global y se usa en todas las subclases de esta clase. Asignarla
+         * solo una vez en el singleton de la app, teniendo en cuenta que la app debe tener configurado Firebase Crashlytics,
+         * o bien dejar en null si no se requiere o si no se usa Firebase Crashlytics en la app. El registro de errores se realiza
+         * solo en ciertos casos donde puede ser util, por lo que donde se dese usar en las subclases de Base, invocar
+         * firebaseCrashlyticsInstance?.recordException(e) para registrar la excepción, ya que no se hace en las funciones de log
+         * directamente porque solo se requiere registrar la excepción en ciertos casos.
+         * */
+        var firebaseCrashlyticsInstance: FirebaseCrashlytics? = null
     }
 
     /**
