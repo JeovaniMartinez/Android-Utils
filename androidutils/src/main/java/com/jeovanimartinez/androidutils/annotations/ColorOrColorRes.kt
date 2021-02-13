475e7336c5fc48ca4741ee88f1acc2fb013f4848
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-13 17:50:03 -0600

    Ajuste de documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/ColorOrColorRes.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/ColorOrColorRes.kt
@@ -1,8 +1,9 @@
 package com.jeovanimartinez.androidutils.annotations
 
 /**
- * Indica que el valor esperado debe ser un color Int o el ID de un recuso de color.
- * Por ejemplo: PENDIENTE
+ * Indica que el valor esperado debe ser un color-int o el ID de un recuso de color.
+ * Por ejemplo: para el color negro, se puede usar Color.parseColor("#000000") que da como resultado -16777216 que es su representación
+ * en color-int, o bien R.color.demo para usar un color de color res.
  * */
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
