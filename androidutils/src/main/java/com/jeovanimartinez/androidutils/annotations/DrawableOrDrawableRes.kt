d30f02a0306b5091a937a6ee4fc2981963dad683
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-13 11:20:18 -0600

    Se agregan y ajustan anotaciones

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/DrawableOrDrawableRes.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/DrawableOrDrawableRes.kt
@@ -1,10 +1,10 @@
 package com.jeovanimartinez.androidutils.annotations
 
 /**
- * Indica que el valor esperado debe ser un dato tipo String o el ID de un recuso de string.
- * Por ejemplo: “Hola”, R.string.demo.
+ * Indica que el valor esperado debe ser un Drawable o el ID de un recuso de drawable.
+ * Por ejemplo: drawableObject, R.drawable.demo.
  * */
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.LOCAL_VARIABLE)
-annotation class StringOrStringRes
+annotation class DrawableOrDrawableRes
