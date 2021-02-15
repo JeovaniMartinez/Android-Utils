abafdae07374232d9f697d7a521a0e5457ffcd17
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-15 12:14:33 -0600

    Se actualiza documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/StringOrStringRes.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/annotations/StringOrStringRes.kt
@@ -1,8 +1,8 @@
 package com.jeovanimartinez.androidutils.annotations
 
 /**
- * Indica que el valor esperado debe ser un dato tipo String o el ID de un recuso de string.
- * Por ejemplo: “Hola”, R.string.demo.
+ * Indica que el valor esperado debe ser un dato tipo String, Char (ya que se puede representar como String) o el ID de un recuso de string.
+ * Por ejemplo: 'a', "Hola", R.string.demo.
  * */
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
