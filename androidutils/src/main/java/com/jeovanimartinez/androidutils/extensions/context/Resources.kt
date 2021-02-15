220f991e20d8879e4ea1808143b00c98501ad23c
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-14 18:07:56 -0600

    Se agregan extensiones para obtener los recursos más fácilmente

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/context/Resources.kt
@@ -0,0 +1,25 @@
+@file:Suppress("unused")
+
+package com.jeovanimartinez.androidutils.extensions.context
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+
+/**
+ * Conjunto de extensiones para trabajar con los recursos dentro del context.
+ * */
+
+/** Obtiene un color de los recursos en base a su id mediante ContextCompat.getColor() */
+@ColorInt
+fun Context.getColorCompat(@ColorRes id: Int): Int {
+    return ContextCompat.getColor(this, id)
+}
+
+/** Obtiene un drawable de los recursos en base a su id mediante ContextCompat.getDrawable() */
+fun Context.getDrawableCompat(@DrawableRes id: Int): Drawable? {
+    return ContextCompat.getDrawable(this, id)
+}
