4a7222a52c12f8bb0cb06963b0be51b76c128654
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-14 19:21:38 -0600

    Se mejora la manera de configurar el task description

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/activity/TaskDescription.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/activity/TaskDescription.kt
@@ -6,10 +6,10 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.graphics.BitmapFactory
 import android.os.Build
-import androidx.annotation.ColorRes
+import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.core.content.res.ResourcesCompat
+import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
+import com.jeovanimartinez.androidutils.extensions.context.typeAsString
 
 /**
  * Extensión para para configurar el TaskDescription de las actividades
@@ -17,31 +17,27 @@ import androidx.core.content.res.ResourcesCompat
 
 /**
  * Configura la TaskDescription de la actividad, funciona desde Android 5 en adelante y ajusta la configuración
- * automáticamente para las diferentes versiones de Android
- * @param title título para la descripción (id del recurso). Debe ser una imagen PNG, si el id del recurso es un SVG, no se va a aplicar
- *              y se va a mostrar el ícono de la app.
- * @param icon ícono a mostrar (id del recurso)
- * @param color color de fondo de la barra (id del recurso)
+ * automáticamente para las diferentes versiones de Android.
+ * @param title título para la descripción, string o id del recurso.
+ * @param icon ícono a mostrar (id del recurso), debe ser una imagen PNG, si el id del recurso es un SVG, no se va a aplicar
+ *        y se va a mostrar el ícono de la app.
+ * @param color color-int de fondo para la barra.
  * */
-fun Activity.configureTaskDescription(@StringRes title: Int, @DrawableRes icon: Int, @ColorRes color: Int) {
+fun Activity.configureTaskDescription(@StringOrStringRes title: Any, @DrawableRes icon: Int, @ColorInt color: Int) {
+
+    val finalTitle = typeAsString(title)
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
 
         // Para Android Pie (9.0) API 28 y versiones posteriores
-
-        val taskDescription = ActivityManager.TaskDescription(getString(title), icon, ResourcesCompat.getColor(resources, color, this.theme))
+        val taskDescription = ActivityManager.TaskDescription(finalTitle, icon, color)
         this.setTaskDescription(taskDescription)
 
     } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 
         // De Android 5.0 a Android 8.1
-
         @Suppress("DEPRECATION")
-        val taskDescription = ActivityManager.TaskDescription(
-            getString(title),
-            BitmapFactory.decodeResource(resources, icon),
-            ResourcesCompat.getColor(resources, color, this.theme)
-        )
+        val taskDescription = ActivityManager.TaskDescription(finalTitle, BitmapFactory.decodeResource(resources, icon), color)
         this.setTaskDescription(taskDescription)
 
     }
