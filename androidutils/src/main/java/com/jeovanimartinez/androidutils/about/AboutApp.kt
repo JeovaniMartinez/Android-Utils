0db2b8a7ac8d206459005721501212dc0d89dcb3
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-15 19:21:21 -0600

    Actualización de implementación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutApp.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/about/AboutApp.kt
@@ -1,100 +1,74 @@
+@file:Suppress("CanBePrimaryConstructorProperty")
+
 package com.jeovanimartinez.androidutils.about
 
 import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Build
-import androidx.annotation.ColorRes
+import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import com.jeovanimartinez.androidutils.Base
 import com.jeovanimartinez.androidutils.R
+import com.jeovanimartinez.androidutils.annotations.DrawableOrDrawableRes
+import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
+import com.jeovanimartinez.androidutils.extensions.context.getColorCompat
 
-/** Utilidad para mostrar una pantalla de acerca de la aplicación */
-object AboutApp : Base<AboutApp>() {
+
+/**
+ * Utilidad para mostrar una actividad de acerca de la aplicación.
+ * @param activity Actividad para poder iniciar la AboutActivity.
+ * @param backgroundColor Color de fondo de la actividad, el valor predeterminado es el color de fondo del tema.
+ * @param iconsColor Color para los iconos, el valor predeterminado es el color de definido en el tema.
+ * @param appIcon Ícono o logo de la aplicación.
+ * @param appName Nombre de la aplicación.
+ * @param appVersionName Version de la aplicación.
+ * @param authorName Nombre del autor.
+ * @param authorLink URL que se abre al dar clic al nombre del autor, null para no abrir ningún enlace.
+ * @param companyLogo Logo de la empresa.
+ * @param companyName Nombre de la empresa.
+ * @param companyLink URL que se abre al dar clic al logo de la empresa, null para no abrir ningún enlace.
+ * @param termsAndPrivacyPolicyLink URL de los términos de uso y política de privacidad, dejar en null si no se desea mostrarlos.
+ * @param termsAndPrivacyPolicyTextColor Color del texto de los términos de uso y política de privacidad, el valor predeterminado es el color de definido en el tema.
+ * @param showOpenSourceLicenses Determina si se muestra un botón para ver las licencias de código abierto de la app. En caso de ser afirmativo, el build.gradle
+ *        del proyecto debe tener la dependencia 'com.google.android.gms:oss-licenses-plugin:VERSION' y el build.gradle del proyecto debe implementar el plugin
+ *        'com.google.android.gms.oss-licenses-plugin', tal como se muestra en la documentación oficial: https://developers.google.com/android/guides/opensource
+ * @param taskDescriptionTitle Título para la Task Description de la actividad. Se usa en conjunto con taskDescriptionIcon y taskDescriptionColor, ninguno de estos tres parámetros
+ *        debe ser null para que se configure el Task Description.
+ * @param taskDescriptionIcon Ícono para la Task Description de la actividad. Se usa en conjunto con taskDescriptionTitle y taskDescriptionColor, ninguno de estos tres parámetros
+ *        debe ser null para que se configure el Task Description.
+ * @param taskDescriptionColor Color para la Task Description de la actividad. Se usa en conjunto con taskDescriptionTitle y taskDescriptionIcon, ninguno de estos tres parámetros
+ *        debe ser null para que se configure el Task Description.
+ * */
+class AboutApp(
+    private val activity: Activity,
+    @ColorInt val backgroundColor: Int = activity.getColorCompat(R.color.colorBackground),
+    @ColorInt val iconsColor: Int = activity.getColorCompat(R.color.colorIcon),
+    @DrawableOrDrawableRes val appIcon: Any,
+    @StringOrStringRes val appName: Any,
+    @StringOrStringRes val appVersionName: Any,
+    @StringOrStringRes val authorName: Any,
+    @StringOrStringRes val authorLink: Any?,
+    @DrawableOrDrawableRes val companyLogo: Any,
+    @StringOrStringRes val companyName: Any,
+    @StringOrStringRes val companyLink: Any?,
+    @StringOrStringRes val termsAndPrivacyPolicyLink: Any?,
+    @ColorInt val termsAndPrivacyPolicyTextColor: Int = activity.getColorCompat(R.color.colorTermsAndPrivacyPolicyText),
+    val showOpenSourceLicenses: Boolean,
+    @StringOrStringRes val taskDescriptionTitle: Any?,
+    @DrawableRes taskDescriptionIcon: Int?,
+    @ColorInt val taskDescriptionColor: Int?
+) : Base<AboutApp>() {
 
     override val LOG_TAG = "AboutApp"
 
-    /** Color de fondo de la actividad, si no se asigna se usa el color de fondo del tema */
-    @ColorRes
-    var backgroundColor = R.color.colorBackground
+    companion object {
+        lateinit var instance: AboutApp
+    }
 
-    /** Color para los iconos */
-    @ColorRes
-    var iconsColor = R.color.colorIcon
-
-    /** Ícono o logo de la aplicación */
-    @DrawableRes
-    var appIcon = R.drawable.library_logo
-
-    /** Nombre de la aplicación */
-    @StringRes
-    var appName = R.string.about_app_app_name
-
-    /** Version de la aplicación */
-    var appVersionName: String = ""
-
-    /** Nombre del autor */
-    @StringRes
-    var authorName = R.string.about_app_author_name
-
-    /** URL que se abre al dar clic al nombre del autor, null para no abrir ningún enlace */
-    @StringRes
-    var authorLink: Int? = R.string.about_app_author_link
-
-    /** Logo de la empresa */
-    @DrawableRes
-    var companyLogo = R.drawable.logo_jedemm_com
-
-    /** Nombre de la empresa */
-    @StringRes
-    var companyName = R.string.about_app_company_name
-
-    /** URL que se abre al dar clic al logo de la empresa, null para no abrir ningún enlace */
-    @StringRes
-    var companyLink: Int? = R.string.about_app_company_link
-
-    /** URL de los términos de uso y política de privacidad, dejar en null si la app no se desea mostrarlos */
-    @StringRes
-    var termsAndPrivacyPolicyLink: Int? = R.string.about_app_terms_and_policy_link
-
-    /** Color del texto de los términos de uso y política de privacidad, si no se define, se usa el color predeterminado  */
-    @ColorRes
-    var termsAndPrivacyPolicyTextColor = R.color.colorTermsAndPrivacyPolicyText
-
-    /**
-     * Determina si se muestra un botón para ver las licencias de código abierto de la app.
-     * En caso de ser afirmativo, el build.gradle del proyecto debe tener la dependencia 'com.google.android.gms:oss-licenses-plugin:VERSION'
-     * y el build.gradle del proyecto debe implementar el plugin 'com.google.android.gms.oss-licenses-plugin', tal como se muestra en la documentación
-     * oficial: https://developers.google.com/android/guides/opensource
-     * */
-    var showOpenSourceLicenses = true
-
-    // Para que se pueda aplicar la task description en la actividad, taskDescriptionTitle, taskDescriptionIcon y taskDescriptionColor deben asignarse
-
-    /**
-     * Título para la Task Description de la actividad. Se usa en conjunto con taskDescriptionIcon y taskDescriptionColor y ninguna
-     * propiedad debe ser null para que se configure el Task Description.
-     * */
-    @StringRes
-    var taskDescriptionTitle: Int? = null
-
-    /**
-     * Icono para la Task Description de la actividad. Se usa en conjunto con taskDescriptionTitle y taskDescriptionColor y ninguna
-     * propiedad debe ser null para que se configure el Task Description.
-     * */
-    @DrawableRes
-    var taskDescriptionIcon: Int? = null
-
-    /**
-     * Color para la Task Description de la actividad. Se usa en conjunto con taskDescriptionTitle y taskDescriptionIcon y ninguna
-     * propiedad debe ser null para que se configure el Task Description.
-     * */
-    @ColorRes
-    var taskDescriptionColor: Int? = null
-
-    /** Muestra la actividad de acerca de, requiere la [activity] para poder iniciar la actividad AboutActivity */
-    fun show(activity: Activity) {
+    /** Muestra la actividad de acerca de */
+    fun show() {
+        instance = this
         activity.startActivity(
             Intent(activity, AboutActivity::class.java),
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) ActivityOptions.makeSceneTransitionAnimation(activity).toBundle() else null
