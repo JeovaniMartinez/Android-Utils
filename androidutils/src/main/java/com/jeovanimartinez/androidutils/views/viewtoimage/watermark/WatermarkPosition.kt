722aa65f3424db92264dc8b612d44e98f2b3f901
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-16 13:14:40 -0600

    Se agrega enum de posiciones para la marca de agua

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/WatermarkPosition.kt
@@ -0,0 +1,16 @@
+@file:Suppress("unused")
+
+package com.jeovanimartinez.androidutils.views.viewtoimage.watermark
+
+/** Enum de posiciones para la marca de agua */
+enum class WatermarkPosition {
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    MIDDLE_LEFT,
+    MIDDLE_CENTER,
+    MIDDLE_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT,
+}
