1dc246085f0ee63afcde75d14f87e4cad021b278
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-16 13:43:23 -0600

    Ajuste de propiedades

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImageConfig.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImageConfig.kt
@@ -1,13 +1,20 @@
 package com.jeovanimartinez.androidutils.views.viewtoimage
 
+import android.content.Context
+import android.view.View
+import androidx.annotation.ColorInt
+import com.jeovanimartinez.androidutils.views.viewtoimage.watermark.TextWatermark
 
-
+/**
+ * Configuración para convertir una vista en una imagen.
+ * @param context Contexto.
+ * @param view Vista que se va a convertir a imagen.
+ * @param backgroundColor Color de fondo a aplicar a la imagen, null aplica un color transparente.
+ * @param textWatermark Marca de agua de texto a colocar en la imagen.
+ * */
 data class ViewToImageConfig(
-
-    /** Color de fondo, debe ser el color en hexadecimal (string) o el id del recurso de colors (int) */
-    var backgroundColor: Any,
-
-
+    val context: Context,
+    val view: View,
+    @ColorInt val backgroundColor: Int? = null,
+    val textWatermark: TextWatermark? = null
 )
-
-
