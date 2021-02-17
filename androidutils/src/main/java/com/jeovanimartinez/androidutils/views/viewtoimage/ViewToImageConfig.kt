f6ac4f992266a0f86eef63f63b2f5cf7dc2bd42c
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-17 17:19:42 -0600

    Se actualiza documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImageConfig.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImageConfig.kt
@@ -11,7 +11,8 @@ import com.jeovanimartinez.androidutils.views.viewtoimage.watermark.TextWatermar
  * Configuración para convertir una vista en una imagen.
  * @param view Vista que se va a convertir a imagen.
  * @param backgroundColor Color de fondo a aplicar a la imagen, el predeterminado es transparente.
- * @param padding Padding a aplicar entre la vista y los bordes de la imagen.
+ * @param padding Padding a aplicar entre la vista y los bordes de la imagen. Los valores se interpretan como dp,
+ *        por ejemplo si el padding top = 10.0, al generar la imagen el padding top será de 10dp.
  * @param textWatermarkList Lista de marcas de agua de texto, dejar la lista vacía para no aplicar ninguna.
  * @param drawableWatermarkList Lista de marcas de agua de drawable, dejar la lista vacía para no aplicar ninguna.
  * */
