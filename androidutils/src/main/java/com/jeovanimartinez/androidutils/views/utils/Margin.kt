6bf0168c9a517594aba4bbd88350e76237595a12
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-17 12:06:58 -0600

    Se agregan clases para manejar margin y padding

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/utils/Margin.kt
@@ -0,0 +1,25 @@
+package com.jeovanimartinez.androidutils.views.utils
+
+/**
+ * Configuración de márgenes.
+ * @param top Margen superior.
+ * @param right Margen derecho.
+ * @param bottom Margen inferior.
+ * @param left Margen izquierdo.
+ * */
+data class Margin(var top: Float, var right: Float, var bottom: Float, var left: Float) {
+
+    /**
+     * Configuración de márgenes.
+     * @param all Valor para aplicar a todos los márgenes (superior, derecho, inferior e izquierdo).
+     * */
+    constructor(all: Float) : this(all, all, all, all)
+
+    /**
+     * Configuración de márgenes.
+     * @param vertical Valor para el margen superior e inferior.
+     * @param horizontal Valor para el margen derecho e izquierdo.
+     * */
+    constructor(vertical: Float, horizontal: Float) : this(vertical, horizontal, vertical, horizontal)
+
+}
---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/utils/Padding.kt
@@ -0,0 +1,25 @@
+package com.jeovanimartinez.androidutils.views.utils
+
+/**
+ * Configuración de rellenos (padding).
+ * @param top Padding superior.
+ * @param right Padding derecho.
+ * @param bottom Padding inferior.
+ * @param left Padding izquierdo.
+ * */
+data class Padding(var top: Float, var right: Float, var bottom: Float, var left: Float) {
+
+    /**
+     * Configuración de rellenos (padding).
+     * @param all Valor para aplicar a todos los padding (superior, derecho, inferior e izquierdo).
+     * */
+    constructor(all: Float) : this(all, all, all, all)
+
+    /**
+     * Configuración de rellenos (padding).
+     * @param vertical Valor para el padding superior e inferior.
+     * @param horizontal Valor para el padding derecho e izquierdo.
+     * */
+    constructor(vertical: Float, horizontal: Float) : this(vertical, horizontal, vertical, horizontal)
+
+}
