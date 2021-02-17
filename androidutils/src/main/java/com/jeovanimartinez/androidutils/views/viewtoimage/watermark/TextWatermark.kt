7eecb058e077bc8fbb9552bd010fb4c6dc29d995
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-17 12:45:43 -0600

    Ajuste de propiedades de TextWatermark

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
@@ -1,18 +1,25 @@
 package com.jeovanimartinez.androidutils.views.viewtoimage.watermark
 
 import androidx.annotation.ColorInt
+import androidx.annotation.FloatRange
 import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
+import com.jeovanimartinez.androidutils.views.utils.Margin
 
 /**
  * Configuración para crear una marca de agua de texto
  * @param text Texto.
  * @param textColor Color del texto.
- * @param position Posición del texto.
+ * @param position Posición del texto dentro de la imagen.
+ * @param margin Márgenes para el texto.
  * @param rotation Rotación del texto.
+ * @param opacity Opacidad del texto en un valor de 0 a 1, donde 0 significa que el texto es completamente transparente
+ *        y 1 significa que el texto es completamente opaco.
  * */
 data class TextWatermark(
     @StringOrStringRes val text: Any,
     @ColorInt val textColor: Int,
     val position: WatermarkPosition,
-    val rotation: Float = 0f
+    val margin: Margin,
+    val rotation: Float = 0f,
+    @FloatRange(from = 0.0, to = 1.0) val opacity: Float = 1f,
 )
