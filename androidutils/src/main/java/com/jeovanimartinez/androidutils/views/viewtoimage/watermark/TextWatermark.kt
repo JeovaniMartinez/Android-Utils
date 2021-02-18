4d8b0b60b8e6b3d83157438d99e17c47fac39773
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-18 11:46:29 -0600

    Ajuste de propiedades

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
@@ -2,28 +2,30 @@ package com.jeovanimartinez.androidutils.views.viewtoimage.watermark
 
 import androidx.annotation.ColorInt
 import androidx.annotation.FloatRange
+import androidx.annotation.FontRes
 import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
-import com.jeovanimartinez.androidutils.views.utils.Margin
 
 /**
- * Configuración para crear una marca de agua de texto
- * @param text Texto.
- * @param textSize Tamaño del texto. El valor se interpreta como sp, por ejemplo si textSize = 12, al generar la marca de agua el tamaño
- *        del texto será de 12 sp.
- * @param textColor Color del texto.
- * @param position Posición del texto dentro de la imagen.
- * @param margin Márgenes para el texto. Los valores se interpretan como dp, por ejemplo si el margin top = 10, al generar la marca de agua
- *        el margin top será de 10 dp.
+ * Configuración para crear una marca de agua de texto.
+ * @param text Texto para la marca de agua.
+ * @param textSize Tamaño del texto.
+ * @param textColor Color-int del texto.
+ * @param position Posición de la marca de agua dentro de la imagen.
+ * @param offsetX Desplazamiento en el eje x.
+ * @param offsetY Desplazamiento en el eje y.
  * @param rotation Rotación del texto.
  * @param opacity Opacidad del texto en un valor de 0 a 1, donde 0 significa que el texto es completamente transparente
  *        y 1 significa que el texto es completamente opaco.
+ * @param typeface Fuente para el texto, null aplica la fuente predeterminada.
  * */
 data class TextWatermark(
     @StringOrStringRes val text: Any,
     val textSize: Float,
     @ColorInt val textColor: Int,
     val position: WatermarkPosition,
-    val margin: Margin,
-    val rotation: Float = 0f,
+    val offsetX: Float,
+    val offsetY: Float,
+    var rotation: WatermarkRotation = WatermarkRotation.DEG_0,
     @FloatRange(from = 0.0, to = 1.0) val opacity: Float = 1f,
+    @FontRes val typeface: Int? = null
 )
