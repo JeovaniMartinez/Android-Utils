bae0d67628e51091c54d75afe53ca48018282c33
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-18 12:26:37 -0600

    Se agrega función de extensión

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/context/Resources.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/context/Resources.kt
@@ -3,10 +3,12 @@
 package com.jeovanimartinez.androidutils.extensions.context
 
 import android.content.Context
+import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.FontRes
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.res.ResourcesCompat
 
@@ -23,4 +25,9 @@ fun Context.getColorCompat(@ColorRes id: Int): Int {
 /** Obtiene un drawable de los recursos en base a su id mediante AppCompatResources.getDrawable() */
 fun Context.getDrawableCompat(@DrawableRes id: Int): Drawable? {
     return AppCompatResources.getDrawable(this, id)
+}
+
+/** Obtiene una fuente de los recursos en base a su id mediante ResourcesCompat.getFont() */
+fun Context.getFontCompat(@FontRes id: Int) : Typeface? {
+    return ResourcesCompat.getFont(this, id)
 }
