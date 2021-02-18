1eeb68840b9b5f9401189498b3b82fe90ec948ef
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-18 12:09:02 -0600

    Ajuste para poder especificar el nombre del archivo a guardar en saveBitmapToFile

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/tempfiles/TempFiles.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/tempfiles/TempFiles.kt
@@ -45,21 +45,25 @@ object TempFiles : Base<TempFiles>() {
     }
 
     /**
-     * Guarda la imagen [bitmap] en un archivo temporal en la carpeta TEMP_FILES_DIR, el nombre
-     * del archivo es un UUID generado y en formato PNG, al final regresa el archivo creado. Para imágenes pequeñas
-     * el proceso es rápido y puede llamarse de manera síncrona, para imágenes grandes se recomienda llamar de manera
-     * asíncrona.
-     * IMPORTANTE: Llamar siempre dentro de un bloque try catch.
+     * Guarda la imagen en un archivo en la carpeta TEMP_FILES_DIR con formato PNG.
+     * Para imágenes pequeñas el proceso es rápido y puede llamarse de manera síncrona,
+     * para imágenes grandes se recomienda llamar de manera asíncrona.
+     * **IMPORTANTE**: Llamar siempre dentro de un bloque try catch.
+     * @param context Contexto.
+     * @param bitmap Imagen tipo bitmap a guardar en el archivo.
+     * @param fileName Nombre del archivo donde se va a guardar la imagen, el parámetro es opcional, si no se
+     *        especifica o si se recibe una cadena en blanco, se genera un UUID para el nombre del archivo.
+     * @return El archivo generado.
      * */
-    fun saveBitmapToFile(context: Context, bitmap: Bitmap): File {
+    fun saveBitmapToFile(context: Context, bitmap: Bitmap, fileName: String = ""): File {
 
-        val fileName = "${UUID.randomUUID()}.png" // Se genera un nombre para el archivo
+        val finalFileName = if (fileName.trim().isBlank()) "${UUID.randomUUID()}.png" else "${fileName.trim()}.png"
 
-        log("Saving bitmap [$fileName] into temp files dir")
+        log("Saving bitmap [$finalFileName] into temp files dir")
         makeTempDir(context) // Se crea el directorio por si no existe
 
         // Se genera el archivo en la ruta de archivos de la app
-        val file = File(context.filesDir, "$TEMP_FILES_DIR/$fileName")
+        val file = File(context.filesDir, "$TEMP_FILES_DIR/$finalFileName")
 
         val stream = ByteArrayOutputStream() // Se crea un output stream para guardar el archivo
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream) // Se asigna el formato y la calidad y se mandan los datos al stream
