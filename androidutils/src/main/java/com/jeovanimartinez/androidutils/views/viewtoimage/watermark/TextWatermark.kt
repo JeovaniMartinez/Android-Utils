cfa6e59bfc71f5e97fc337876ce666f4d2a85f12
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-20 18:48:46 -0600

    Se ajusta documentación

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/watermark/TextWatermark.kt
@@ -8,17 +8,16 @@ import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
 /**
  * Configuración para crear una marca de agua de texto.
  * @param text Texto para la marca de agua.
- * @param textSize Tamaño del texto. El tamaño del texto se interpreta en dp, por ejemplo si textSize = 12f, al generar la
- *        marca de agua es como si textSize fuera 12dp. Se interpreta como dp en lugar de sp para mantener el tamaño, ya que si
- *        el dispositivo usa un tamaño de fuente más grande generaría que la marca de agua estuviese más grande, y esto sería
- *        un inconveniente, ya que, por ejemplo, si se deja un margen en la imagen de 20dp para la marca de agua, y el tamaño
- *        del texto fuese de 20f (20sp), si el dispositivo muestra la fuente tamaño normal no habría problema, pero si usa una fuente
- *        más grande, el texto de la marca de agua no cabria en el margen, así que interpretando el valor como dp no se genera
- *        este inconveniente.
+ * @param textSize Tamaño del texto. El tamaño del texto se interpreta en dp, por ejemplo si textSize = 12f, al generar la marca de agua es como
+ *        si textSize fuera 12dp. Se interpreta como dp en lugar de sp para mantener el tamaño, ya que si el dispositivo usa un tamaño de fuente
+ *        más grande o más pequeña, el tamaño del texto de la marca de agua variaría, y esto sería un inconveniente, ya que, por ejemplo, si se
+ *        deja un margen en la imagen de 20dp para la marca de agua, y el tamaño del texto fuese de 20f (20sp), si el dispositivo muestra la fuente
+ *        tamaño normal no habría problema, pero si usa una fuente más grande, el texto de la marca de agua no cabria en el margen, así que interpretando
+ *        el valor como dp no se genera este inconveniente.
  * @param textColor Color-int del texto.
  * @param position Posición de la marca de agua dentro de la imagen.
- * @param offsetX Desplazamiento en el eje x.
- * @param offsetY Desplazamiento en el eje y.
+ * @param offsetX Desplazamiento en el eje x, el valor se interpreta como dp, por ejemplo, si offsetX = 10f se interpreta como offsetX = 10dp.
+ * @param offsetY Desplazamiento en el eje y, el valor se interpreta como dp, por ejemplo, si offsetY = 10f se interpreta como offsetY = 10dp.
  * @param rotation Rotación del texto.
  * @param opacity Opacidad del texto en un valor de 0 a 1, donde 0 significa que el texto es completamente transparente
  *        y 1 significa que el texto es completamente opaco.
