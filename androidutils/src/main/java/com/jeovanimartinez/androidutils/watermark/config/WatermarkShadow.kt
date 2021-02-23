52b83e2c23ccbc656feda8c5ec4726833eef920d
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-23 11:52:51 -0600

    Added documentation for WatermarkShadow

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/watermark/config/WatermarkShadow.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/watermark/config/WatermarkShadow.kt
@@ -2,6 +2,13 @@ package com.jeovanimartinez.androidutils.watermark.config
 
 import androidx.annotation.ColorInt
 
+/**
+ * Configuration to create a shadow for watermark.
+ * @param radius Shadow blur radius.
+ * @param dx Shadow offset for x-axis.
+ * @param dy Shadow offset for y-axis.
+ * @param shadowColor Color-int for the shadow, can be apply alpha for transparent shadow.
+ * */
 data class WatermarkShadow(
     val radius: Float,
     val dx: Float,
