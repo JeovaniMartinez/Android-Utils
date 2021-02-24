bd27f59f491296e69e7a9e0363384c33ba6641e6
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-02-23 18:07:38 -0600

    Finished watermark class definition

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/watermark/Watermark.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/watermark/Watermark.kt
@@ -1,10 +1,14 @@
 package com.jeovanimartinez.androidutils.watermark
 
-import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.annotation.ColorInt
 import androidx.annotation.FloatRange
-import com.jeovanimartinez.androidutils.Dimension
+import androidx.annotation.FontRes
+import com.jeovanimartinez.androidutils.graphics.utils.Dimension
 import com.jeovanimartinez.androidutils.annotations.DrawableOrDrawableRes
+import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
 import com.jeovanimartinez.androidutils.watermark.config.WatermarkPosition
+import com.jeovanimartinez.androidutils.watermark.config.WatermarkShadow
 
 /**
  * Definition class for a watermark.
@@ -16,36 +20,69 @@ sealed class Watermark {
     abstract val dy: Float
     abstract val rotation: Float
     abstract val opacity: Float
-    abstract val measureDimension: Dimension
+    abstract val measurementDimension: Dimension
 
     /**
      * Definition for a drawable watermark (shape, image, etc.)
      * @param drawable Drawable resource or object for the watermark.
      * @param position Position for the watermark inside of the image.
-     * @param width Width for the watermark if the value is negative, the intrinsic width of the drawable will be used.
-     *        If the drawable intrinsic width is also negative, an exception will be thrown.
-     * @param height Height for the watermark if the value is negative, the intrinsic height of the drawable will be used.
-     *        If the drawable intrinsic height is also negative, an exception will be thrown.
+     * @param width Width for the watermark if the value is zero or negative, the intrinsic width of the drawable will be used.
+     *        If the drawable intrinsic width is also zero or negative, an exception will be thrown.
+     * @param height Height for the watermark if the value is zero or negative, the intrinsic height of the drawable will be used.
+     *        If the drawable intrinsic height is also zero or negative, an exception will be thrown.
      * @param dx Watermark offset for the x-axis.
      * @param dy Watermark offset for the y-axis.
      * @param rotation Rotation in degrees for the watermark.
-     * @param opacity Opacity of the watermark. This is a value from 0 to 1, where 0 means the view is completely
-     *        transparent and 1 means the view is completely opaque.
-     * @param measureDimension Type of dimension to use to draw watermark, its apply for width, height, dx and dy.
-     *        For example, if the value of width is 10f and measureDimension is Dimension.PX, the width of the drawn watermark
-     *        will be 10 px, but if measureDimension is Dimension.DP, the the width of the drawn watermark will be calculate
+     * @param opacity Opacity of the watermark. This is a value from 0 to 1, where 0 means the watermark is completely
+     *        transparent and 1 means the watermark is completely opaque.
+     * @param measurementDimension Type of dimension to use to draw watermark, its apply for width, height, dx and dy.
+     *        For example, if the value of width is 10f and measurementDimension is Dimension.PX, the width of the drawn watermark
+     *        will be 10 px, but if measurementDimension is Dimension.DP, the the width of the drawn watermark will be calculate
      *        according the screen density.
      * */
     data class Drawable(
         @DrawableOrDrawableRes val drawable: Any,
-        override val position: WatermarkPosition,
-        val width: Float = -1f,
-        val height: Float = -1f,
+        override val position: WatermarkPosition = WatermarkPosition.MIDDLE_CENTER,
+        val width: Float = 0f,
+        val height: Float = 0f,
         override val dx: Float = 0f,
         override val dy: Float = 0f,
         override val rotation: Float = 0f,
         @FloatRange(from = 0.0, to = 1.0) override val opacity: Float = 1f,
-        override val measureDimension: Dimension = Dimension.PX
+        override val measurementDimension: Dimension = Dimension.PX
+    ) : Watermark()
+
+
+    /**
+     * Definition for a text watermark.
+     * @param text Text for the watermark.
+     * @param textSize Text size for the watermark.
+     * @param textColor Color-int for the watermark text.
+     * @param position Position for the watermark inside of the image.
+     * @param dx Watermark offset for the x-axis.
+     * @param dy Watermark offset for the y-axis.
+     * @param rotation Rotation in degrees for the watermark.
+     * @param opacity Opacity of the watermark. This is a value from 0 to 1, where 0 means the watermark is completely
+     *        transparent and 1 means the watermark is completely opaque.
+     * @param typeface Custom font for the watermark text, if value is null applies default font.
+     * @param shadow Shadow configuration for text watermark, null for not apply shadow.
+     * @param measurementDimension Type of dimension to use to draw watermark, its apply for textSize, dx, dy and shadow configuration (radius, dx and dy).
+     *        For example, if the value of dx is 10f and measurementDimension is Dimension.PX, the offset of the drawn watermark will be 10 px in x-axis,
+     *        but if measurementDimension is Dimension.DP, the the offset of the drawn watermark in x-axis will be calculate
+     *        according the screen density.
+     * */
+    data class Text(
+        @StringOrStringRes val text: Any,
+        val textSize: Float = 12f,
+        @ColorInt val textColor: Int = Color.BLACK,
+        override val position: WatermarkPosition = WatermarkPosition.MIDDLE_CENTER,
+        override val dx: Float = 0f,
+        override val dy: Float = 0f,
+        override val rotation: Float = 0f,
+        @FloatRange(from = 0.0, to = 1.0) override val opacity: Float = 1f,
+        @FontRes val typeface: Int? = null,
+        val shadow: WatermarkShadow? = null,
+        override val measurementDimension: Dimension = Dimension.PX
     ) : Watermark()
 
 }
