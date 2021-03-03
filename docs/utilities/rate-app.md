5cc22586ee78ca2bea183563ee3239083d7f6f47
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-02 21:18:51 -0600

    Added RateApp docs

---
+++ docs/utilities/rate-app.md
@@ -0,0 +1,24 @@
+---
+id: rate-app
+title: RateApp
+---
+
+#### <a href="../reference/androidutils/com.jeovanimartinez.androidutils.reviews/-rate-app/index.html" target="_blank"><b>[ Reference ]</b></a>
+
+### Description
+
+Utility to direct the user to app details on Google Play, usually used to invite the user to rate the app.
+
+![img](../img/rate-app/rate-app-img1.png)
+
+:::note
+If the Google Play app is not available in the device, the user is directed to app details to Google Play website on the system web browser.
+:::
+
+---
+
+### Usage
+
+```kotlin
+RateApp.goToRateInGooglePlay(activity)
+```
--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
         Introduction: ['introduction'],
         Configuration: ['configuration/getting-started', 'configuration/log', 'configuration/firebase-analytics', 'configuration/firebase-crashlytics'],
         Annotations: ['annotations/code-inspection-annotations'],
-        "Library Utilities": ['utilities/translucent-theme', 'utilities/rate-in-app'],
+        "Library Utilities": ['utilities/translucent-theme', 'utilities/rate-in-app', 'utilities/rate-app'],
         "Release Notes": ['release-notes/v1.0.0'],
         Docusaurus: ['docusaurus/style-guide', 'docusaurus/mdx'], // Temporally
     },
