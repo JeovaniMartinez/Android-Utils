3198863c6a4d05098eba3869d4d5be028d7a6d6c
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-05 20:54:47 -0600

    Optimize conversion and update documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/graphics/MarginAndPadding.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/graphics/MarginAndPadding.kt
@@ -3,36 +3,41 @@
 package com.jeovanimartinez.androidutils.extensions.graphics
 
 import android.content.Context
-import com.jeovanimartinez.androidutils.extensions.dimension.dp2px
 import com.jeovanimartinez.androidutils.graphics.utils.Margin
 import com.jeovanimartinez.androidutils.graphics.utils.Padding
 
 /**
- * Conjunto de extensiones para las clases Margin y Padding
+ * Set of extensions for the Margin and Padding classes.
  * */
 
 /**
- * Convierte todos los valores del margen de píxeles independientes de la densidad (dp) a píxeles (px) y los devuelve en un nuevo objeto.
- * Por ejemplo, si el valor de margin top = 10, se va a asumir que este valor esta representado en dp, por lo que se realiza la conversión
- * a píxeles de acuerdo a la densidad de la pantalla, y el resultado de esa conversión será el margin top del objeto que se devuelve.
+ * Converts all density-independent pixels (dp) margin values to pixels (px) and returns them to a new object.
+ * > For example, if the value of margin top = 10, it will be assumed that this value is represented in dp, so
+ * the conversion to pixels is carried out according to the density of the screen, and the result of that
+ * conversion will be the margin top of the returned object.
+ * @param context Context
+ * @return A new Margin object with the converted values.
  * */
 fun Margin.dp2px(context: Context): Margin {
-    val top = context.dp2px(this.top)
-    val right = context.dp2px(this.right)
-    val bottom = context.dp2px(this.bottom)
-    val left = context.dp2px(this.left)
-    return Margin(top.toFloat(), right.toFloat(), bottom.toFloat(), left.toFloat())
+    val top = this.top * context.resources.displayMetrics.density
+    val right = this.right * context.resources.displayMetrics.density
+    val bottom = this.bottom * context.resources.displayMetrics.density
+    val left = this.left * context.resources.displayMetrics.density
+    return Margin(top, right, bottom, left)
 }
 
 /**
- * Convierte todos los valores del margen de píxeles independientes de la densidad (dp) a píxeles (px) y los devuelve en un nuevo objeto.
- * Por ejemplo, si el valor de margin top = 10, se va a asumir que este valor esta representado en dp, por lo que se realiza la conversión
- * a píxeles de acuerdo a la densidad de la pantalla, y el resultado de esa conversión será el margin top del objeto que se devuelve.
+ * Converts all density-independent pixels (dp) padding values to pixels (px) and returns them to a new object.
+ * > For example, if the value of padding top = 10, it will be assumed that this value is represented in dp, so
+ * the conversion to pixels is carried out according to the density of the screen, and the result of that
+ * conversion will be the padding top of the returned object.
+ * @param context Context
+ * @return A new Padding object with the converted values.
  * */
 fun Padding.dp2px(context: Context): Padding {
-    val top = context.dp2px(this.top)
-    val right = context.dp2px(this.right)
-    val bottom = context.dp2px(this.bottom)
-    val left = context.dp2px(this.left)
-    return Padding(top.toFloat(), right.toFloat(), bottom.toFloat(), left.toFloat())
+    val top = this.top * context.resources.displayMetrics.density
+    val right = this.right * context.resources.displayMetrics.density
+    val bottom = context.resources.displayMetrics.density
+    val left = context.resources.displayMetrics.density
+    return Padding(top, right, bottom, left)
 }
