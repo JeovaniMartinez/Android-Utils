765c23374602544e7ed3ffa185f57057f5415a18
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-05 20:55:52 -0600

    Update documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/basictypes/Float.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/basictypes/Float.kt
@@ -11,12 +11,10 @@ package com.jeovanimartinez.androidutils.extensions.basictypes
  * Re-maps a number from one range to another. That is, a value of [inMin] would get mapped to [outMin],
  * a value of [inMax] to [outMax], values in-between to values in-between, etc.
  *
- * ```
- * For example, inMin = 0, inMax = 1, outMin = 0, outMin = 255 - In this case, the input value
+ * > For example, inMin = 0, inMax = 1, outMin = 0, outMin = 255 - In this case, the input value
  * must be between 0 and 1, and it will be mapped to an output between 0 and 255. If the value
  * input is 0.5, output is 127.5. In this same example, a value of 2 would result in 510, since
  * it is double according to the established values.
- * ```
  *
  * @param inMin The lower bound of the value’s current range.
  * @param inMax The upper bound of the value’s current range.
