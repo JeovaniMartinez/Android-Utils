f6c0779bd9bf2e7b0338665af867e941b163f171
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-06 13:18:51 -0600

    Update documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/moreapps/MoreAppsGPlay.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/moreapps/MoreAppsGPlay.kt
@@ -9,31 +9,30 @@ import com.jeovanimartinez.androidutils.R
 import com.jeovanimartinez.androidutils.extensions.context.shortToast
 
 /**
- * Utilidad para dirigir al usuario a Google Play, específicamente a la lista de aplicaciones del desarrollador.
- * El uso principal es para invitar al usuario a que instale otras aplicaciones del desarrollador.
+ * Utility to direct the user to the app developer page on Google Play
+ * Usually used to invite the user to install the developer apps.
  * */
 object MoreAppsGPlay : Base<MoreAppsGPlay>() {
 
     override val LOG_TAG = "MoreAppsGPlay"
 
-    /** ID del desarrollador del cual se va a mostrar la lista de aplicaciones al llamar a showAppList */
+    /** Developer ID whose list of apps is to be displayed when calling showAppList. */
     var developerId = "Jedemm+Technologies"
 
     /**
-     * Dirige al usuario a la lista de aplicaciones del desarrollador, en base a su ID (developerId).
-     * Si no es posible, muestra un toast con un mensaje.
+     * Directs the user to the list of developer apps, based on their ID (developerId). If not possible, show a toast with a message.
      *
-     * @param activity actividad desde donde se llama, se usa para iniciar otras actividades
+     * @param activity Activity.
      * */
     fun showAppList(activity: Activity) {
         try {
-            // Se intenta abrir en el navegador web la página del desarrollador (si el usuario tiene Google Play se muestra ahí)
+            // An attempt is made to open the developer's page in the system web browser (if the user has Google Play app, it is shown there)
             val webUriString = "https://play.google.com/store/apps/developer?id=$developerId"
             activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(webUriString)))
             log("Sent user to view developer page in google play [$webUriString]")
             firebaseAnalytics("more_apps_sent_to_google_play", null)
         } catch (e: ActivityNotFoundException) {
-            // Si no se pudo mostrar, se muestra un mensaje
+            // If it couldn't be shown developer's app list, a message is displayed into a toast
             activity.shortToast(R.string.more_apps_unable_to_show_dev_page)
             logw("Unable to send user to developer page", e)
             firebaseAnalytics("more_apps_unable_to_show_dev_page", null)
