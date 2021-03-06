a4bdc1f8d8f1fcea0100f56d4282b6a3066744b4
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-05 19:12:11 -0600

    Add TaskDescriptionConfig

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/activity/config/TaskDescriptionConfig.kt
@@ -0,0 +1,14 @@
+package com.jeovanimartinez.androidutils.activity.config
+
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
+
+/**
+ * Class for configuration of activity TaskDescription.
+ *
+ * @param title Title for the task description, string or resource ID.
+ * @param icon Icon to show (resource id), it must be a PNG image, if the resource id is SVG, it will not be applied and the app icon will be shown.
+ * @param color Color-int for the background.
+ * */
+data class TaskDescriptionConfig(@StringOrStringRes val title: Any, @DrawableRes val icon: Int, @ColorInt val color: Int)
