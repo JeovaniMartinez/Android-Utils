314862108ed90ff844211c96f3d3bbee0f6d290a
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-05 18:49:28 -0600

    Update documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/context/TypeOrResource.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/context/TypeOrResource.kt
@@ -10,40 +10,39 @@ import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
 import java.lang.Exception
 
 /**
- * Conjunto de extensiones para tratar un valor como un tipo de dato, o bien
- * obtener el valor de los recursos.
+ * Set of extensions to parse a value as a data type, or to get the value from resources.
  * */
 
 /**
- * Analiza el tipo de dato recibido [stringOrStringRes] y devuelve siempre un string.
- * - Si el tipo de dato es string lo devuelve tal cual, sin procesar.
- * - Si el tipo de dato es char, lo devuelve como string.
- * - Si el tipo de dato es entero, se asume que es un ID de recurso, por lo que se obtiene y devuelve el texto, en caso de no existir el recurso, lanza una excepción.
- * - Para cualquier otro tipo de dato lanza una excepción, ya que el dato no se puede tratar directamente como string.
+ * Parses the received data type [stringOrStringRes] and always returns a String.
+ * - If the data type is String, it returns it as is, without processing.
+ * - If the data type is char, it returns it as a String.
+ * - If the data type is an integer, it is assumed to be a resource ID, so the String is obtained from resources and returned. If the resource does not exist, it throws an exception.
+ * - For any another data type, it throws an exception, since the data type cannot be parse directly as a String.
  * */
 fun Context.typeAsString(@StringOrStringRes stringOrStringRes: Any): String {
     return when (stringOrStringRes) {
-        is String -> stringOrStringRes // Si es string, se devuelve tal cual
-        is Char -> stringOrStringRes.toString() // Si es char se devuelve como string
-        is Int -> this.getString(stringOrStringRes) // Si es un entero, se asume que es el ID de un recurso por lo que se obtiene, si el recurso no existe, se genera una excepción
-        else -> throw Exception("Expected value type string object or int (for get the string resource by ID) but received value type ${stringOrStringRes.javaClass}") // Tipo de dato no válido
+        is String -> stringOrStringRes
+        is Char -> stringOrStringRes.toString()
+        is Int -> this.getString(stringOrStringRes)
+        else -> throw Exception("Expected value type String object or Int (for get the string resource by ID) but received value type ${stringOrStringRes.javaClass}")
     }
 }
 
 /**
- * Analiza el tipo de dato recibido [drawableOrDrawableRes] y devuelve siempre un Drawable.
- * - Si el tipo de dato es null, devuelve null.
- * - Si el tipo de dato es Drawable lo devuelve tal cual, sin procesar.
- * - Si el tipo de dato es entero, se asume que es un ID de recurso, por lo que se obtiene y devuelve el Drawable, en caso de no existir el recurso, lanza una excepción.
- * - Para cualquier otro tipo de dato lanza una excepción, ya que el dato no se puede tratar directamente como Drawable.
+ * Parses the received data type [drawableOrDrawableRes] and always returns a Drawable.
+ * - If the data type is null, it returns null.
+ * - If the data type is a Drawable, it returns it as is, without processing.
+ * - If the data type is an integer, it is assumed to be a resource ID, so the Drawable is obtained from resources and returned. If the resource does not exist, it throws an exception.
+ * - For any another data type, it throws an exception, since the data type cannot be parse as a Drawable.
  *
- * El valor de retorno puede ser null de acuerdo a la definición de ContextCompat.getDrawable()
+ * The parameter and return value can be null according to the definition of AppCompatResources.getDrawable()
  * */
 fun Context.typeAsDrawable(@DrawableOrDrawableRes drawableOrDrawableRes: Any?): Drawable? {
-    if (drawableOrDrawableRes == null) return null // Si es null, devuelve null
+    if (drawableOrDrawableRes == null) return null
     return when (drawableOrDrawableRes) {
-        is Drawable -> drawableOrDrawableRes // Si es drawable, se devuelve tal cual
-        is Int -> AppCompatResources.getDrawable(this, drawableOrDrawableRes) // Si es un entero, se asume que es el ID de un recurso
-        else -> throw Exception("Expected value type drawable object or int (for get the drawable resource by ID) but received value type ${drawableOrDrawableRes.javaClass}") // Tipo de dato no válido
+        is Drawable -> drawableOrDrawableRes
+        is Int -> AppCompatResources.getDrawable(this, drawableOrDrawableRes)
+        else -> throw Exception("Expected value type Drawable object or Int (for get the drawable resource by ID) but received value type ${drawableOrDrawableRes.javaClass}")
     }
 }
