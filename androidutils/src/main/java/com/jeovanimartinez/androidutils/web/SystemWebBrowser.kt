cce3a3480062a077ac36ce6362f5e8b5b848f827
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-09 18:33:03 -0600

    Update documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/web/SystemWebBrowser.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/web/SystemWebBrowser.kt
@@ -13,41 +13,40 @@ import com.jeovanimartinez.androidutils.extensions.context.shortToast
 import java.lang.Exception
 
 /**
- * Utilidad para interactuar con el navegador web del sistema
+ * Utility to interact with the system web browser.
  * */
 object SystemWebBrowser : Base<SystemWebBrowser>() {
 
     override val LOG_TAG = "SystemWebBrowser"
 
     /**
-     * Abre el navegador web del sistema en la [url] especificada
-     * @param context contexto para poder lanzar el intent del navegador
-     * @param url URL a mostrar, debe ser una URL completa, que incluya http o https, de otro modo no va a pasar la validación
-     * @param case razón por la que se abre la URL en el navegador. Esto aplica solo si Firebase Analytics esta habilitado, cuando se muestra
-     *        una URL en el navegador se registra el evento, este evento contiene un parámetro que ayuda a determinar que sitio web se mostró,
-     *        y se registra el valor de case. Debe tener entre 1 y 100 caracteres.
+     * Open the system web browser at the specified [url].
+     * @param context Context.
+     * @param url URL to show, it must be a complete URL, including http or https, otherwise it will not pass the validation
+     * @param case Reason that the URL was opens in the browser. This applies only if Firebase Analytics is enabled.
+     *        When a URL is loaded in the browser the event is registered, this event contains a parameter that
+     *        helps determine which website was shown.
      * */
     fun openUrl(context: Context, url: String, @Size(min = 1L, max = 100L) case: String? = null) {
-        // Se valida la URL
+
         if (!URLUtil.isValidUrl(url)) return logw("The URL [$url] is not a valid URL")
 
         try {
-            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url))) // Se inicia la actividad
+            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+            // The event is logged
             if (case != null && case.isNotBlank()) {
-                // Se registra el evento y el caso como parámetro
                 firebaseAnalytics("open_url_in_system_web_browser", Bundle().apply { putString("open_url_case", case.trim()) })
             } else {
-                // Solo se registra el evento
                 firebaseAnalytics("open_url_in_system_web_browser")
             }
             log("URL: [$url] opened, case: ${case?.trim()}")
         } catch (e: ActivityNotFoundException) {
-            // No Hay ninguna aplicación que pueda abrir la URL
+            // There is no app that can open the URL
             context.shortToast(R.string.system_web_browser_not_available)
             firebaseAnalytics("open_url_in_system_web_browser", Bundle().apply { putString("open_url_case", "activity_not_found_exception") })
             logw("Unable to open URL [$url], web browser not available", e)
         } catch (e: Exception) {
-            // Excepción general
+            // General exception
             context.shortToast(R.string.system_web_browser_error)
             firebaseAnalytics("open_url_in_system_web_browser", Bundle().apply { putString("open_url_case", "exception") })
             loge("Error opening URL [$url]", e)
