92a57c2fbb1e068fd7cdb08289769cd9c99d771f
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-20 17:23:35 -0600

    Adjust documentation

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/config/ExcludeMode.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/config/ExcludeMode.kt
@@ -3,30 +3,30 @@
 package com.jeovanimartinez.androidutils.views.viewtoimage.config
 
 /**
- * Sets the mode to exclude a children view when converting a view group to an image.
+ * Sets the mode to exclude a child view when converting a view group to an image.
  * */
 enum class ExcludeMode {
 
     /**
-     * Hides the children view by replacing all the space it occupies with the background color of the view,
+     * Hides the child view by replacing all the space it occupies with the background color of the view,
      * the image of the view keeps its original size.
      * */
     HIDE,
 
     /**
-     * Crops the image in the view vertically, deleting all the space occupied by the children view, so the
+     * Crops the image of the view vertically, deleting all the space occupied by the child view, so the
      * resulting image will have a smaller height than the original view.
      * */
     CROP_VERTICALLY,
 
     /**
-     * Crops the image in the view horizontally, deleting all the space occupied by the children view, so the
+     * Crops the image of the view horizontally, deleting all the space occupied by the child view, so the
      * resulting image will have a smaller width than the original view.
      * */
     CROP_HORIZONTALLY,
 
     /**
-     * Crops the image in the view vertically and horizontally, deleting all the space occupied by the children
+     * Crops the image of the view vertically and horizontally, deleting all the space occupied by the child
      * view, so the resulting image will have a smaller height and width than the original view.
      * */
     CROP_ALL,
