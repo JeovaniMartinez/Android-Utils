d2700195bf8fce8664db1e2d2689cd56233d03f2
Jeovani Martínez <jeovani_martinez@hotmail.com>
2021-03-23 18:26:20 -0600

    Adjust convert function

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImage.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/views/viewtoimage/ViewToImage.kt
@@ -9,6 +9,7 @@ import androidx.annotation.ColorInt
 import androidx.core.graphics.applyCanvas
 import androidx.core.view.*
 import com.jeovanimartinez.androidutils.Base
+import com.jeovanimartinez.androidutils.extensions.graphics.trimByBorderColor
 import com.jeovanimartinez.androidutils.filesystem.FileUtils
 import com.jeovanimartinez.androidutils.graphics.utils.Margin
 import com.jeovanimartinez.androidutils.graphics.utils.Padding
@@ -58,162 +59,15 @@ object ViewToImage : Base<ViewToImage>() {
             throw IllegalStateException("View needs to be laid out before calling drawToBitmap()")
         }
 
+        var viewBitmap = excludeChildrenViews(context, view, viewsToExclude)
 
-        val viewBitmap = excludeChildrenViews(context, view, viewsToExclude)
+        FileUtils.saveBitmapToFile(context, viewBitmap, "CONVERT_STEP_0") // *** FOR DEVELOPMENT PURPOSES ONLY
 
-        FileUtils.saveBitmapToFile(context, viewBitmap, "step1")
+        viewBitmap = viewBitmap.trimByBorderColor(Color.parseColor("#F0F000"))
 
-        val viewCanvas = Canvas(viewBitmap)
+        FileUtils.saveBitmapToFile(context, viewBitmap, "CONVERT_STEP_1") // *** FOR DEVELOPMENT PURPOSES ONLY
 
-        return Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
-
-
-        //val excludeViewPaint = Paint().apply { style = Paint.Style.FILL; xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR) }
-        val excludeViewPaint = Paint().apply { style = Paint.Style.FILL; color = Color.RED; } // For development purposes only
-
-        var heExclude = 0f
-
-        // Replace the view location with transparent pixels, including margin and padding
-        viewsToExclude.forEach {
-            if (it.view.visibility == View.GONE) return@forEach
-            // NOTE: The padding is part of the view, so it is already included in the width or height
-            viewCanvas.drawRect(
-                0f,//it.view.x - it.view.marginLeft,
-                it.view.y - it.view.marginTop,
-                viewBitmap.width.toFloat(),//it.view.x + it.view.width + it.view.marginRight,
-                it.view.y + it.view.height + it.view.marginBottom,
-                excludeViewPaint
-            )
-
-            heExclude += it.view.height + it.view.marginTop + it.view.marginBottom
-        }
-
-        var colored = IntArray(viewBitmap.width) { Color.RED }
-        var buffer = IntArray(viewBitmap.width)
-
-        val b = Bitmap.createBitmap(viewBitmap.width, (viewBitmap.height - heExclude).toInt(), Bitmap.Config.ARGB_8888)
-
-        var y = 0
-
-        for (i in 0 until viewBitmap.height) {
-            viewBitmap.getPixels(buffer, 0, viewBitmap.width, 0, i, viewBitmap.width, 1)
-            if (!colored.contentEquals(buffer)) {
-                b.setPixels(buffer, 0, viewBitmap.width, 0, y, viewBitmap.width, 1)
-                y++
-            }
-        }
-
-        FileUtils.saveBitmapToFile(context, viewBitmap, "original")
-        FileUtils.saveBitmapToFile(context, b, "proceced")
-
-
-        val verticallyCrop = viewsToExclude.filter { it.mode == ExcludeMode.CROP_VERTICALLY }.sortedBy { it.view.y }
-
-        var croppedBitmap = Bitmap.createBitmap(viewBitmap.width, viewBitmap.height, Bitmap.Config.ARGB_8888)
-        Canvas(croppedBitmap).drawBitmap(viewBitmap, 0f, 0f, null)
-
-        var ya = false
-
-        var h = 0
-
-        /*verticallyCrop.forEach {
-            if (!ya) {
-
-                var topBitmap: Bitmap? = null
-
-                var topBitmapHeight = (it.view.y - it.view.marginTop - h).toInt()
-                if (topBitmapHeight > 0) {
-                    topBitmap = Bitmap.createBitmap(croppedBitmap.width, topBitmapHeight, Bitmap.Config.ARGB_8888)
-                    Canvas(topBitmap).drawBitmap(croppedBitmap, 0f, 0f -h, null)
-
-                    FileUtils.saveBitmapToFile(context, topBitmap, "top")
-                }
-
-                if (topBitmapHeight < 0) topBitmapHeight = 0
-
-                var bottomBitmap: Bitmap? = null
-                var bottomBitmapHeight = croppedBitmap.height - topBitmapHeight - it.view.height - it.view.marginTop - it.view.marginBottom
-                if (bottomBitmapHeight > 0) {
-                    bottomBitmap = Bitmap.createBitmap(croppedBitmap.width, bottomBitmapHeight, Bitmap.Config.ARGB_8888)
-                    Canvas(bottomBitmap).drawBitmap(croppedBitmap, 0f, -(it.view.y + it.view.marginBottom + it.view.height), null)
-
-                    FileUtils.saveBitmapToFile(context, bottomBitmap, "bottom")
-                }
-
-                if (bottomBitmapHeight < 0) bottomBitmapHeight = 0
-
-                croppedBitmap = Bitmap.createBitmap(croppedBitmap.width, topBitmapHeight + bottomBitmapHeight, Bitmap.Config.ARGB_8888)
-                val canvas = Canvas(croppedBitmap)
-
-                topBitmap.whenNotNull { bitmap ->
-                    canvas.drawBitmap(bitmap, 0f, 0f - h, null)
-                }
-
-                bottomBitmap.whenNotNull { bitmap ->
-                    canvas.drawBitmap(bitmap, 0f, topBitmapHeight.toFloat() - h, null)
-                }
-
-                h = it.view.height - it.view.marginTop - it.view.marginBottom
-
-                //ya = true
-            }
-        }*/
-
-
-        //val horizontallyCrop = viewsToExclude.filter { it.cropImageHorizontally }.sortedBy { it.view.x }
-
-
-        // TMPPP
-
-
-        //log("Finished process to convert a view to bitmap image")
-
-
-        //if (viewsToExclude.size > 0) log("We proceed to exclude ${viewsToExclude.size} views") else log("There are no views to exclude")
-
-        /*
-        val imageWidth = (view.width + padding.left + padding.right).toInt()
-        val imageHeight = (view.height + padding.top + padding.bottom).toInt()
-
-        /*if (!ViewCompat.isLaidOut(view)) {
-            throw IllegalStateException("View needs to be laid out before calling drawToBitmap()")
-        }*/
-
-
-        val viewBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888).applyCanvas {
-
-            translate(-view.scrollX.toFloat(), -view.scrollY.toFloat())
-            drawColor(backgroundColor)
-            view.draw(this)
-        }
-
-        val image = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888)
-
-        val canvas = Canvas(image)
-
-
-        canvas.drawBitmap(viewBitmap, padding.left, padding.top, null)
-
-
-        val obj = view.findViewById<View>(id)
-
-        val shape = GradientDrawable()
-            .apply { shape = GradientDrawable.RECTANGLE; setColor(Color.RED) }
-
-        WatermarkUtils.drawWatermark(
-            context, image, Watermark.Drawable(
-                drawable = shape,
-                position = WatermarkPosition.TOP_LEFT,
-                width = obj.width.toFloat() + obj.marginStart + obj.marginEnd,
-                height = obj.height.toFloat() + obj.marginStart + obj.marginEnd,
-                dx = obj.x - obj.marginStart,
-                dy = obj.y - obj.marginTop,
-                rotation = 0f,
-                opacity = 1f,
-                measurementDimension = Dimension.PX
-            )
-        )
-*/
+        log("Finished process to convert a view to a bitmap image")
 
         return Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888)
     }
@@ -251,7 +105,7 @@ object ViewToImage : Base<ViewToImage>() {
             }
         }
 
-        // FileUtils.saveBitmapToFile(context, view.drawToBitmap(Bitmap.Config.ARGB_8888), "STEP_0") // *** FOR DEVELOPMENT PURPOSES ONLY
+        // FileUtils.saveBitmapToFile(context, view.drawToBitmap(Bitmap.Config.ARGB_8888), "EXCLUDE_VIEWS_STEP_0") // *** FOR DEVELOPMENT PURPOSES ONLY
 
         log("Started process to exclude children views")
 
@@ -307,7 +161,7 @@ object ViewToImage : Base<ViewToImage>() {
             }
             log("Excluded ${hideViews.size} view(s) from the image with mode ExcludeMode.HIDE")
 
-            // FileUtils.saveBitmapToFile(context, viewBitmap, "STEP_1") // *** FOR DEVELOPMENT PURPOSES ONLY
+            // FileUtils.saveBitmapToFile(context, viewBitmap, "EXCLUDE_VIEWS_STEP_1") // *** FOR DEVELOPMENT PURPOSES ONLY
         }
 
         // STEP 2, EXCLUDE VIEWS WITH CROP VERTICALLY
@@ -347,8 +201,8 @@ object ViewToImage : Base<ViewToImage>() {
             }
             log("Excluded ${cropVerticallyViews.size} view(s) from the image with mode crop vertically")
 
-            // FileUtils.saveBitmapToFile(context, viewBitmap, "STEP_2_A") // *** FOR DEVELOPMENT PURPOSES ONLY
-            // FileUtils.saveBitmapToFile(context, viewBitmap2, "STEP_2_B") // *** FOR DEVELOPMENT PURPOSES ONLY
+            // FileUtils.saveBitmapToFile(context, viewBitmap, "EXCLUDE_VIEWS_STEP_2_A") // *** FOR DEVELOPMENT PURPOSES ONLY
+            // FileUtils.saveBitmapToFile(context, viewBitmap2, "EXCLUDE_VIEWS_STEP_2_B") // *** FOR DEVELOPMENT PURPOSES ONLY
         }
 
         // STEP 3, EXCLUDE VIEWS WITH CROP HORIZONTALLY
@@ -389,13 +243,13 @@ object ViewToImage : Base<ViewToImage>() {
             }
             log("Excluded ${cropHorizontallyViews.size} view(s) from the image with mode crop horizontally")
 
-            // FileUtils.saveBitmapToFile(context, viewBitmap2, "STEP_3_A") // *** FOR DEVELOPMENT PURPOSES ONLY
-            // FileUtils.saveBitmapToFile(context, viewBitmap3, "STEP_3_B") // *** FOR DEVELOPMENT PURPOSES ONLY
+            // FileUtils.saveBitmapToFile(context, viewBitmap2, "EXCLUDE_VIEWS_STEP_3_A") // *** FOR DEVELOPMENT PURPOSES ONLY
+            // FileUtils.saveBitmapToFile(context, viewBitmap3, "EXCLUDE_VIEWS_STEP_3_B") // *** FOR DEVELOPMENT PURPOSES ONLY
         }
 
         // ExcludeMode.CROP_ALL its processed in cropVerticallyViews and cropHorizontallyViews
 
-        // FileUtils.saveBitmapToFile(context, viewBitmap3, "STEP_NA_RESULT") // *** FOR DEVELOPMENT PURPOSES ONLY
+        // FileUtils.saveBitmapToFile(context, viewBitmap3, "EXCLUDE_VIEWS_RESULT") // *** FOR DEVELOPMENT PURPOSES ONLY
 
         log("Finished process to exclude children views")
 
