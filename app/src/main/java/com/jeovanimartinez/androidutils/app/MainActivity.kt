8178f07f1ceb62c61b5939a2180c04213d5ed764
Jeovani Martínez <jeovani_martinez@hotmail.com>
2022-07-12 18:01:01 -0500

    Main activity design, part 1

--- app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/MainActivity.kt
@@ -2,10 +2,12 @@ package com.jeovanimartinez.androidutils.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.jeovanimartinez.androidutils.app.databinding.ActivityMainBinding
 import com.jeovanimartinez.androidutils.extensions.activity.configureTaskDescription
 import com.jeovanimartinez.androidutils.extensions.context.getColorCompat
 
+/** MainActivity */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -17,10 +19,24 @@ class MainActivity : AppCompatActivity() {
         configureTaskDescription(R.string.app_name, R.mipmap.ic_launcher, getColorCompat(R.color.colorBackground))
 
         initialSetup()
+        themeSetup()
     }
 
     /** Initial setup */
     private fun initialSetup() {
+
+    }
+
+    /** Theme Setup */
+    private fun themeSetup() {
+
+        binding.btnToggleTheme.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
 
     }
 
