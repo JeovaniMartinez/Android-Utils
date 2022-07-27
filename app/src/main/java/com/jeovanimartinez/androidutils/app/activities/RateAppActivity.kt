f0742fc510d26ab19cfd8883e599b1af85fcbb6d
Jeovani Martínez <jeovani_martinez@hotmail.com>
2022-07-26 20:18:17 -0500

    Adjust binding

--- app/src/main/java/com/jeovanimartinez/androidutils/app/activities/RateAppActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/activities/RateAppActivity.kt
@@ -2,7 +2,6 @@ package com.jeovanimartinez.androidutils.app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jeovanimartinez.androidutils.app.R
 import com.jeovanimartinez.androidutils.app.databinding.ActivityRateAppBinding
 
 /** RateAppActivity */
@@ -13,7 +12,8 @@ class RateAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRateAppBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_rate_app)
+        setContentView(binding.root)
+
     }
 
 }
