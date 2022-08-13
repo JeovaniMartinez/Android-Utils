623ec16d880a45c7db842897a07cdbecef796ea1
Jeovani Martínez <jeovani_martinez@hotmail.com>
2022-08-12 19:51:47 -0500

    Add base activity

---
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/activities/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.jeovanimartinez.androidutils.app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jeovanimartinez.androidutils.app.R
+import com.jeovanimartinez.androidutils.app.databinding.ActivityBaseBinding
+
+/** Base activity for a new utility */
+class BaseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.appBar.btnBack.setOnClickListener { super.onBackPressed() }
+        binding.appBar.tvTitle.text = getString(R.string.app_name)
+
+    }
+
+}
