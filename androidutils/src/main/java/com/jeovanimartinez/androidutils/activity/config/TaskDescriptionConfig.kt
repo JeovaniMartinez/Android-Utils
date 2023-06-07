dc34bb3a2120f01e42f912542e89d89dde29b0eb
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-06-06 20:45:56 -0600

    Improvements and optimization to apply task description configuration only when necessary

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/activity/config/TaskDescriptionConfig.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/activity/config/TaskDescriptionConfig.kt
@@ -5,10 +5,11 @@ import androidx.annotation.DrawableRes
 import com.jeovanimartinez.androidutils.annotations.StringOrStringRes
 
 /**
- * Class for configuration of activity TaskDescription.
+ * Class for the configuration of activity TaskDescription.
+ * * IMPORTANT: This class represents only a basic configuration to use when the app runs on Android Oreo (8.1 API 27) and earlier versions of Android.
  *
- * @param title Title for the task description, String, or string resource ID.
+ * @param title Title for the current activity to show in the task description.
  * @param icon Icon to show (resource id), it must be a PNG image, if the resource id is an SVG, it will not be applied and the app icon will be shown.
- * @param color Color-int for the background.
+ * @param color A color to override the theme's primary color, this color must be opaque.
  * */
 data class TaskDescriptionConfig(@StringOrStringRes val title: Any, @DrawableRes val icon: Int, @ColorInt val color: Int)
--- androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/activity/TaskDescription.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/extensions/activity/TaskDescription.kt
@@ -3,7 +3,7 @@
 package com.jeovanimartinez.androidutils.extensions.activity
 
 import android.app.Activity
-import android.app.ActivityManager
+import android.app.ActivityManager.TaskDescription
 import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.ColorInt
@@ -17,39 +17,35 @@ import com.jeovanimartinez.androidutils.extensions.context.typeAsString
  * */
 
 /**
- * Configure the activity TaskDescription, it works from Android 5 onwards and adjusts the config automatically
- * for the different versions of Android.
+ * Configure the activity TaskDescription.
+ * * IMPORTANT: This configuration only applies if the app is running on Android Oreo (8.1 API 27) or earlier versions of Android.
+ *   This is because in newer versions, when viewing the list of running apps, they are displayed differently, and the default Task Description
+ *   settings looks good without the need for adjustments.
  *
- * @param title Title for the task description, String or string resource ID.
+ * @param title Title for the current activity to show in the task description.
  * @param icon Icon to show (resource id), it must be a PNG image, if the resource id is an SVG, it will not be applied and the app icon will be shown.
- * @param color Color-int for the background.
+ * @param color A color to override the theme's primary color, this color must be opaque.
  * */
 fun Activity.configureTaskDescription(@StringOrStringRes title: Any, @DrawableRes icon: Int, @ColorInt color: Int) {
 
-    val finalTitle = typeAsString(title)
+    // From Android Lollipop (5.0 API 21, minSdkVersion of the library) to Android Oreo (8.1 API 27)
+    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O_MR1) {
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        val finalTitle = typeAsString(title)
 
-        // For Android Pie (9.0) API 28 and later versions
-        val taskDescription = ActivityManager.TaskDescription(finalTitle, icon, color)
-        this.setTaskDescription(taskDescription)
-
-    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
-        // From Android 5.0 to Android 8.1
         @Suppress("DEPRECATION")
-        val taskDescription = ActivityManager.TaskDescription(finalTitle, BitmapFactory.decodeResource(resources, icon), color)
+        val taskDescription = TaskDescription(finalTitle, BitmapFactory.decodeResource(resources, icon), color)
         this.setTaskDescription(taskDescription)
 
     }
 
-    // No have support for versions prior to Android 5
-
 }
 
 /**
- * Configure the activity TaskDescription, it works from Android 5 onwards and adjusts the config automatically
- * for the different versions of Android.
+ * Configure the activity TaskDescription.
+ * * IMPORTANT: This configuration only applies if the app is running on Android Oreo (8.1 API 27) or earlier versions of Android.
+ *   This is because in newer versions, when viewing the list of running apps, they are displayed differently, and the default Task Description
+ *   settings looks good without the need for adjustments.
  *
  * @param taskDescriptionConfig Object with the configuration for the activity TaskDescription.
  * */
