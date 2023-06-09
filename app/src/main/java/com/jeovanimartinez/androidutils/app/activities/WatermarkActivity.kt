eedb33efd998630c01aa76c827fcada1c438ea63
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-06-08 19:42:31 -0600

    Add first part for watermark configuration

--- app/src/main/java/com/jeovanimartinez/androidutils/app/activities/WatermarkActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/activities/WatermarkActivity.kt
@@ -2,10 +2,15 @@ package com.jeovanimartinez.androidutils.app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.jeovanimartinez.androidutils.app.R
 import com.jeovanimartinez.androidutils.app.databinding.ActivityWatermarkBinding
 import com.jeovanimartinez.androidutils.extensions.activity.configureTaskDescription
 import com.jeovanimartinez.androidutils.extensions.context.getColorCompat
+import com.jeovanimartinez.androidutils.graphics.utils.Dimension
+import com.jeovanimartinez.androidutils.watermark.config.WatermarkPosition
 
 /** WatermarkActivity */
 class WatermarkActivity : AppCompatActivity() {
@@ -18,6 +23,10 @@ class WatermarkActivity : AppCompatActivity() {
     )
     private var currentImage = 0
 
+    // Common watermark properties
+    private var watermarkPosition = WatermarkPosition.ABSOLUTE
+    private var watermarkMeasurementDimension = Dimension.PX
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWatermarkBinding.inflate(layoutInflater)
@@ -28,7 +37,7 @@ class WatermarkActivity : AppCompatActivity() {
         binding.appBar.tvTitle.text = getString(R.string.watermark_title)
 
         imageActionsSetup()
-
+        commonWatermarkPropertiesSetup()
     }
 
     /** Image actions setup */
@@ -44,6 +53,27 @@ class WatermarkActivity : AppCompatActivity() {
             if (currentImage >= imageList.size - 1) currentImage = 0
             else currentImage++
             binding.ivWatermark.setImageResource(imageList[currentImage])
+        }
+
+    }
+
+    /** Common watermark properties setup */
+    private fun commonWatermarkPropertiesSetup() {
+
+        val positionsMenu = binding.menuPosition.editText as MaterialAutoCompleteTextView
+        val positionsAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.watermark_positions_array))
+        positionsMenu.setAdapter(positionsAdapter)
+        positionsMenu.setText(positionsAdapter.getItem(0).toString(), false)
+        positionsMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            watermarkPosition = WatermarkPosition.values()[position]
+        }
+
+        val measurementDimensionMenu = binding.menuMeasurementDimension.editText as MaterialAutoCompleteTextView
+        val measurementDimensionAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.watermark_dimensions_array))
+        measurementDimensionMenu.setAdapter(measurementDimensionAdapter)
+        measurementDimensionMenu.setText(measurementDimensionAdapter.getItem(0).toString(), false)
+        measurementDimensionMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            watermarkMeasurementDimension = Dimension.values()[position]
         }
 
     }
