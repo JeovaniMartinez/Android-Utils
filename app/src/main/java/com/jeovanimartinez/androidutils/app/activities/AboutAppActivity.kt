2d9c068600cf3a86d70247536a08a4327b219372
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-07-19 18:18:28 -0600

    Add button to toggle theme and to show about app

--- app/src/main/java/com/jeovanimartinez/androidutils/app/activities/AboutAppActivity.kt
+++ app/src/main/java/com/jeovanimartinez/androidutils/app/activities/AboutAppActivity.kt
@@ -1,8 +1,12 @@
 package com.jeovanimartinez.androidutils.app.activities
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.jeovanimartinez.androidutils.app.R
+import com.jeovanimartinez.androidutils.app.constants.Preferences
 import com.jeovanimartinez.androidutils.app.databinding.ActivityAboutAppBinding
 import com.jeovanimartinez.androidutils.extensions.activity.configureTaskDescription
 import com.jeovanimartinez.androidutils.extensions.context.getColorCompat
@@ -11,6 +15,7 @@ import com.jeovanimartinez.androidutils.extensions.context.getColorCompat
 class AboutAppActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAboutAppBinding
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,34 @@ class AboutAppActivity : AppCompatActivity() {
 
         binding.appBar.btnBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
         binding.appBar.tvTitle.text = getString(R.string.about_app_title)
+
+        preferences = getSharedPreferences(Preferences.GENERAL_PREFERENCES_FILE, Context.MODE_PRIVATE)
+
+        generalSetup()
+    }
+
+    /** General setup */
+    private fun generalSetup() {
+
+        binding.btnToggleTheme.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                preferences.edit().putBoolean(Preferences.DARK_THEME_ENABLED, false).apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                preferences.edit().putBoolean(Preferences.DARK_THEME_ENABLED, true).apply()
+            }
+        }
+
+        binding.btnShowAboutAppActivity.setOnClickListener {
+            showAboutAppActivity()
+        }
+
+    }
+
+    /** Shows the about app activity from the library */
+    private fun showAboutAppActivity() {
+
     }
 
 }
