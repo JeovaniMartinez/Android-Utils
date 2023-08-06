08d0c0cafd6325787ce57351a931ed98697ac473
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-05 20:52:31 -0600

    Add EmailUtils file

---
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/email/EmailUtils.kt
@@ -0,0 +1,10 @@
+package com.jeovanimartinez.androidutils.email
+
+import com.jeovanimartinez.androidutils.Base
+
+/** Set of utilities for e-mails. */
+object EmailUtils : Base<EmailUtils>() {
+
+    override val LOG_TAG = "EmailUtils"
+
+}
