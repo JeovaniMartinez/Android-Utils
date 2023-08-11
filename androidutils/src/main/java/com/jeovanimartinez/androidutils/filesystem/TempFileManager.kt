a15c752e10bf1c93dbf3b628c1ae71f4d8caa5a3
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-10 20:53:19 -0600

    Add callback function

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/TempFileManager.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/TempFileManager.kt
@@ -20,9 +20,12 @@ object TempFileManager : Base<TempFileManager>() {
      * Delete all files created with this utility (the files in the temporary folder), it is recommended to call at the
      * start of the app or when you need to clean the contents of the folder. The function runs asynchronously so as
      * not to affect the app flow. It's not necessary to call within a try-catch block, as exceptions are handled internally.
+     * @param context The app context.
+     * @param onComplete Optional callback function that will be invoked when the operation is complete. It will receive a
+     *        boolean parameter indicating the success of the operation.
      * */
     @OptIn(DelicateCoroutinesApi::class)
-    fun clearTempFilesFolder(context: Context) {
+    fun clearTempFilesFolder(context: Context, onComplete: (success: Boolean) -> Unit = {}) {
 
         log("clearTempFilesFolder() invoked")
 
@@ -35,13 +38,16 @@ object TempFileManager : Base<TempFileManager>() {
                 // The directory is created again; makeTempDir() is not used to avoid shown logs
                 dir.mkdirs()
 
-                if (fileCount > 0) log("Deleted $fileCount file(s) from temp files dir")
-                else log("Temp files dir is empty, no need to delete files")
+                if (fileCount > 0) log("Deleted $fileCount file(s) from the temp files dir")
+                else log("The temp files dir is empty, no need to delete files")
 
                 log("clearTempFilesFolder() done")
+                onComplete(true)
+
             } catch (e: Exception) {
-                loge("Failed to delete files from the temporary folder", e)
+                loge("Failed to delete the files from the temporary directory", e)
                 firebaseCrashlyticsInstance?.recordException(e)
+                onComplete(false)
             }
         }
     }
@@ -49,8 +55,9 @@ object TempFileManager : Base<TempFileManager>() {
     /**
      * Create the directory for temporary files (if it does not already exist). Invoke whenever you are going to work with the
      * temporary files directory.
+     * @param context The app context.
      * */
-    fun makeTempDir(context: Context) {
+    internal fun makeTempDir(context: Context) {
         log("makeTempDir() invoked")
         val result = File(context.filesDir, TEMP_FILES_DIR).mkdirs() // Create the directory if it does not exist. If it exists, do not take any action.
         if (result) log("The directory for temporary files is created [${context.filesDir}/$TEMP_FILES_DIR]")
