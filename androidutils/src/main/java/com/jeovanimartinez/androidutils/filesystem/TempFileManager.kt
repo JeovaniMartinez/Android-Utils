73d29f49b8b5ed05871685c3210a6b2a2ca31cda
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-11 18:02:02 -0600

    Add javadoc

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/TempFileManager.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/filesystem/TempFileManager.kt
@@ -12,7 +12,7 @@ import java.util.UUID
 /** Utility to work with temporary files. */
 object TempFileManager : Base<TempFileManager>() {
 
-    /** Directory to store temporary files, the parent is context.filesDir */
+    /** Directory to store temporary files, the parent is context.filesDir (private app directory) */
     const val TEMP_FILES_DIR = "androidutils/tempfiles"
 
     override val LOG_TAG = "TempFileManager"
@@ -21,7 +21,7 @@ object TempFileManager : Base<TempFileManager>() {
      * Delete all files created with this utility (the files in the temporary folder), it is recommended to call at the
      * start of the app or when you need to clean the contents of the folder. The function runs asynchronously so as
      * not to affect the app flow. It's not necessary to call within a try-catch block, as exceptions are handled internally.
-     * @param context The app context.
+     * @param context Context.
      * @param onComplete Optional callback function that will be invoked when the operation is complete. It will receive a
      *        boolean parameter indicating the success of the operation.
      * */
@@ -53,6 +53,26 @@ object TempFileManager : Base<TempFileManager>() {
         }
     }
 
+    /**
+     * Create a new file in the temporary files directory. The file will be generated in the file system until some
+     * content is saved to it, and it will be deleted when [clearTempFilesFolder] is called.
+     * ```
+     *     // Example of the file name based on the received parameters.
+     *     // ** NOTE: An empty string will be interpreted as null, and string are trimmed **
+     *     (fileName = null, fileExtension = null) // f067ee7e-7875-4e4a-9f3b-ddddddf365e5
+     *     (fileName = "demo", fileExtension = null) // demo
+     *     (fileName = "demo.txt", fileExtension = null) // demo.txt
+     *     (fileName = "demo", fileExtension = "txt") // demo.txt
+     *     (fileName = "demo.txt", fileExtension = "txt") // demo.txt.txt
+     *     (fileName = null, fileExtension = "txt") // c1c53230-d6b7-4216-a8a3-a12eb1aec165.txt
+     * ```
+     * @param context Context.
+     * @param fileName File name to create, set as null to use a UUID as the file name, which is highly recommended
+     *        for the temp files folder.
+     * @param fileExtension File extension. Set as null to create a file without extension, or if the extension is
+     *        already included in the [fileName].
+     * @return The newly created temporary file.
+     * */
     fun createNewTempFile(context: Context, fileName: String? = null, fileExtension: String? = null): File {
 
         // Generate the final file name
@@ -69,7 +89,7 @@ object TempFileManager : Base<TempFileManager>() {
     /**
      * Create the directory for temporary files (if it does not already exist). Invoke whenever you are going to work with the
      * temporary files directory.
-     * @param context The app context.
+     * @param context Context.
      * */
     private fun makeTempDir(context: Context) {
         log("makeTempDir() invoked")
