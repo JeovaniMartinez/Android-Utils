f704caa5a632fd0be7cb2c676d19af0ea47dfa45
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-15 17:25:50 -0600

    Rewrite and optimize getSelectedAppName()

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/share/ApplicationSelectorReceiver.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/share/ApplicationSelectorReceiver.kt
@@ -1,14 +1,15 @@
 package com.jeovanimartinez.androidutils.share
 
-import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import com.jeovanimartinez.androidutils.Base
 import com.jeovanimartinez.androidutils.analytics.Event
+import com.jeovanimartinez.androidutils.extensions.nullability.whenNotNull
 import java.util.*
 
 /**
@@ -41,27 +42,47 @@ internal class ApplicationSelectorReceiver : BroadcastReceiver() {
     /**
      * Gets the name of the selected application when sharing.
      * */
-    @SuppressLint("Assert")
     private fun getSelectedAppName(context: Context, intent: Intent): String {
-        var appName = Event.ParameterValue.N_A
+
+        var result = Event.ParameterValue.N_A // Default value
+
         try {
-            @Suppress("UNNECESSARY_NOT_NULL_ASSERTION")
-            for (key in Objects.requireNonNull(intent.extras)!!.keySet()) {
-                try {
-                    val componentInfo = intent.extras!![key] as ComponentName?
-                    val packageManager = context.packageManager
-                    assert(componentInfo != null)
-                    appName = packageManager.getApplicationLabel(packageManager.getApplicationInfo(componentInfo!!.packageName, PackageManager.GET_META_DATA)) as String
-                } catch (e: Exception) {
-                    // It is not necessary to register, it occurs if the information of the intent extra cannot be obtained since it is of another type
+            intent.extras.whenNotNull { intentExtras ->
+                // All keys of the bundle are iterated
+                for (key in intentExtras.keySet()) {
+
+                    // Depending on the Android version, and if the data type is ComponentName, it is assigned to the variable; otherwise, the variable is assigned to null
+                    val componentInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        intentExtras.getParcelable(key, ComponentName::class.java) // It is only assigned if it belongs to the ComponentName::class.java
+                    } else {
+                        @Suppress("DEPRECATION")
+                        intentExtras.get(key) as? ComponentName // It is only assigned if it can be converted to ComponentName
+                    }
+
+                    componentInfo.whenNotNull {
+                        // The app information is obtained
+                        val appInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            context.packageManager.getApplicationInfo(it.packageName, PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong()))
+                        } else {
+                            @Suppress("DEPRECATION")
+                            context.packageManager.getApplicationInfo(it.packageName, PackageManager.GET_META_DATA)
+                        }
+                        result = context.packageManager.getApplicationLabel(appInfo).toString() // Finally, the app name is obtained
+                    }
                 }
             }
         } catch (e: Exception) {
+            // ** Ideally, an exception should never occur with the way this code is written; however, it is used only for safety **
             e.printStackTrace()
             Base.firebaseCrashlyticsInstance?.recordException(e)
         }
-        if (appName.length > 100) appName = appName.subSequence(0, 100).toString() // It is reduced to a maximum of 100 characters to be able to register the event in firebase analytics
-        return appName
+
+        // The result is reduced to a maximum of 100 characters to be able to register the event in firebase analytics
+        if (result.length > 100) {
+            result = result.subSequence(0, 100).toString()
+        }
+
+        return result
     }
 
 }
