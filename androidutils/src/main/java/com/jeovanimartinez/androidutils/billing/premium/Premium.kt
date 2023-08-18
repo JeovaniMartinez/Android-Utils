6ee3a383736733ca2b32c82afeca8151cd84638f
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-17 20:52:17 -0600

    Programing Premium utility, part 1

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/billing/premium/Premium.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/billing/premium/Premium.kt
@@ -2,7 +2,6 @@
 
 package com.jeovanimartinez.androidutils.billing.premium
 
-import com.android.billingclient.api.*
 import com.jeovanimartinez.androidutils.Base
 
 /**
@@ -11,5 +10,42 @@ import com.jeovanimartinez.androidutils.Base
 object Premium : Base<Premium>() {
 
     override val LOG_TAG = "Premium"
+
+    /**
+     * Define the possible states when checking if a user has premium privileges in the app.
+     * */
+    enum class State {
+
+        /**
+         * The user does not have premium privileges.
+         * This indicates that the user has never purchased a premium version of the app, so
+         * should be denied access to premium features.
+         * */
+        NOT_PREMIUM,
+
+        /**
+         * Indicates that the user has started the process of purchasing a premium version of
+         * the app, but the purchase has not yet been completed.
+         * In this case, access to premium features should be denied until the transaction
+         * status changes, whether it is completed or canceled.
+         * */
+        PENDING_TRANSACTION,
+
+        /**
+         * The user has premium privileges.
+         * This indicates that at some point the user has already paid for the purchase of a
+         * premium version of the app, so should be granted access to premium features.
+         * */
+        PREMIUM,
+
+    }
+
+    /**
+     * Object with constants for the preferences.
+     * */
+    private object Preferences {
+        const val FILE_NAME = "aUpPkjY1IcXzlW47AfkB" // Name for the preferences file
+        const val PREMIUM_STATE = "aUpS17v1xp7eOkEMo7pM" // Key to store the last known value of the Premium status
+    }
 
 }
