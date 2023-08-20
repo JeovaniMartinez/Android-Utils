cfd208bbad630066aed7b90ea95bdbd245c30035
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-19 18:27:12 -0600

    Programing Premium utility, part 13

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/billing/premium/Premium.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/billing/premium/Premium.kt
@@ -11,4 +11,54 @@ object Premium : Base<Premium>() {
 
     override val LOG_TAG = "Premium"
 
+    /** Current premium state. */
+    private var currentPremiumState: PremiumState = PremiumState.NOT_PREMIUM // Initial value
+        set(value) {
+            log("Updated Premium.currentPremiumState from $field to $value")
+            field = value
+        }
+
+    /**
+     * Returns the current premium state.
+     * **Its value must be used in all conditions within the app where it is necessary to verify the premium state.**
+     * */
+    fun getCurrentPremiumState(): PremiumState {
+        if (!Controller.initialized) {
+            throw IllegalStateException("It is necessary to call Premium.Controller.init() before being able to retrieve the current premium state")
+        }
+        return currentPremiumState
+    }
+
+    /**
+     * Controller for the purchase and verification process of the premium version of the app.
+     * */
+    object Controller {
+
+        internal var initialized = false // Helper to determine if init was already called
+
+        /**
+         * Initialize and configure the utility. It must be called only once when starting the app.
+         * */
+        fun init() {
+
+            log("Invoked > init()")
+
+            if (initialized) {
+                logw(
+                    """
+                        This call to Premium.Controller.init() HAS BEEN IGNORED AND WILL HAVE NO EFFECT, 
+                        as Premium.Controller.init() should only be called once when the application starts.
+                    """.trimIndent()
+                )
+                return
+            }
+
+            initialized = true
+
+            log("The premium controller has been initialized successfully")
+
+        }
+
+    }
+
 }
