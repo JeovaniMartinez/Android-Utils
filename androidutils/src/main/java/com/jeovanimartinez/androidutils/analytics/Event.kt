c729823bc42dd87ad05213c31caf1c6465c26e2d
Jeovani Martínez <jeovani_martinez@hotmail.com>
2023-08-24 17:38:29 -0600

    Add Events

--- androidutils/src/main/java/com/jeovanimartinez/androidutils/analytics/Event.kt
+++ androidutils/src/main/java/com/jeovanimartinez/androidutils/analytics/Event.kt
@@ -7,33 +7,15 @@ internal object Event {
 
     // Check the library docs for a events and parameters description.
 
-    const val BILLING_CHECK_PREMIUM_CLIENT = "billing_check_premium_client"
-    const val BILLING_CHECK_PREMIUM_PREFERENCES = "billing_check_premium_preferences"
-    const val BILLING_CLIENT_CONNECTION_OK = "billing_client_connection_ok"
-    const val BILLING_CLIENT_CONNECTION_ERROR = "billing_client_connection_error"
-    const val BILLING_CLIENT_DISCONNECTED = "billing_client_disconnected"
-    const val BILLING_SKU_DETAILS_OK = "billing_sku_details_ok"
-    const val BILLING_SKU_DETAILS_ERROR = "billing_sku_details_error"
-    const val BILLING_FLOW_LAUNCH_OK = "billing_flow_launch_ok"
-    const val BILLING_FLOW_LAUNCH_ERROR = "billing_flow_launch_error"
-    const val BILLING_PURCHASE_CANCELLED = "billing_purchase_cancelled"
-    const val BILLING_PURCHASE_COMPLETED = "billing_purchase_completed"
-    const val BILLING_PURCHASE_ACKNOWLEDGE_ERROR = "billing_purchase_acknowledge_error"
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /*
-    * IMPORTANT
-    * The above events are still not fully verified and may change and be adjusted. The events below this comment are
-    * fully ready for the first final version of the library and are being moved from top to bottom as they are reviewed,
-    * validated, and tested.
-    * */
-
     // About App
     const val ABOUT_APP_SHOWN = "about_app_shown"
     const val ABOUT_APP_OSL_SHOWN = "about_app_osl_shown"
     const val ABOUT_APP_TERMS_POLICY_SHOWN = "about_app_terms_policy_shown"
     const val ABOUT_APP_HELP_SECTION_SHOWN = "about_app_help_section_shown"
+
+    // Billing Premium
+    const val PREMIUM_BILLING_LAUNCHED_BILLING_FLOW = "premium_billing_launched_billing_flow"
+    const val PREMIUM_BILLING_PURCHASE_COMPLETED = "premium_billing_purchase_completed"
 
     // Developer Apps
     const val DEV_APPS_SHOWN_LIST_GOOGLE_PLAY = "dev_apps_shown_list_google_play"
